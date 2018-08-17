9b9dbedb6601a2e57687dfac7ff0d373a2feb9b2
jibrazil <jibrazil@populusgroup.com>
2018-08-17 08:17:02 -0400

    Added unified login page

---
+++ Mobile Apps/B2C-Custom-Policy-Templates/Pages/unified.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace B2CCustomPolicyTemplates.Pages
+{
+    public class unifiedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
