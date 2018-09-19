5b412db13fff28e80c0cb23af1bbb5ae19e3ae78
Brazil, Jim <jibrazil@populusgroup.com>
2018-09-19 19:30:59 -0400

    Adding Course Controller

---
+++ Product/API/Controllers/CourseController.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using UpDiddyApi.Models;
+using UpDiddyLib.Dto;
+using UpDiddyLib.MessageQueue;
+
+namespace UpDiddyApi.Controllers
+{
+
+
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+
+        private readonly UpDiddyDbContext _db = null;
+        private readonly IMapper _mapper;
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+        private readonly string _queueConnection = string.Empty;
+        private readonly CCQueue _queue = null;
+        public CourseController(UpDiddyDbContext db, IMapper mapper, Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            _db = db;
+            _mapper = mapper;
+            _configuration = configuration;
+            _queueConnection = _configuration["CareerCircleQueueConnection"];
+            _queue = new CCQueue("ccmessagequeue", _queueConnection);
+        }
+
+        // GET: api/courses
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult Get()
+        {
+
+            IList<CourseDto> rval = null;
+            rval = _db.Course
+                .Where(t => t.IsDeleted == 0)
+                .ProjectTo<CourseDto>()
+                .ToList();
+
+            return Ok(rval);
+
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{TopicSlug}")]
+        public IActionResult Get(string TopicSlug)
+        {
+
+            IList<TopicDto> matchingTopic = _db.Topic.Where(t => t.Slug == TopicSlug).ProjectTo<TopicDto>().ToList();
+            int topicId = 0;
+            foreach (TopicDto topic in matchingTopic)
+            {
+                topicId = topic.TopicId;
+            }
+
+            IList<CourseDto> rval = null;
+            rval = _db.Course
+                .Where(t => t.IsDeleted == 0 && t.TopicId == topicId)
+                .ProjectTo<CourseDto>()
+                .ToList();
+
+            return Ok(rval);
+
+        }
+        // Post: api/course/vendor/coursecode
+        // TODO make Authorized 
+        // [Authorize]
+        // TODO make post 
+        [HttpGet]
+        [Route("api/[controller]/PurchaseCourse/{VendorCode}/{CourseCode}")]
+        public IActionResult PurchaseCourse(string VendorCode, string CourseCode)
+        {
+            // 1) Create Enrollment record
+            // 2) Queue Purchase Course Message 
+            var Msg = new EnrollmentMessage
+            {
+                CourseCode = CourseCode,
+                VendorGuid = VendorCode,
+                Nonce = 1030304343.00
+            };
+            _queue.EnQueue<EnrollmentMessage>(Msg);
+
+            return Ok(CourseCode);
+        }
+
+
+
+        // Post: api/course/vendor/coursecode
+        // TODO make Authorized 
+        // [Authorize]
+        // TODO make post 
+        [HttpGet]
+        [Route("api/[controller]/EnrollStudent/{EnrollmentGuid}")]
+        public IActionResult EnrollStudent(string EnrollmentGuid)
+        {
+
+            // 1) Call WOZ API if users does not have an exeter ID
+            // 2) Return OK
+            return Ok();
+        }
+
+        // Post: api/course/vendor/coursecode
+        // TODO make Authorized 
+        // [Authorize]
+        // TODO make post 
+        [HttpGet]
+        [Route("api/[controller]/CreateSection/{EnrollmentGuid}")]
+        public IActionResult CreateSection(string EnrollmentGuid)
+        {
+            // 1) Call WOZ to see if section exists if not create it 
+            // 2) Return OK
+            return Ok();
+        }
+
+        // Post: api/course/vendor/coursecode
+        // TODO make Authorized 
+        // [Authorize]
+        // TODO make post 
+        [HttpGet]
+        [Route("api/[controller]/EnrollStudentInSection/{EnrollmentGuid}")]
+        public IActionResult EnrollStudentInSection(string EnrollmentGuid)
+        {
+            // 1) 
+            // 2) Return OK
+            return Ok();
+        }
+
+
+
+    }
+
+}
