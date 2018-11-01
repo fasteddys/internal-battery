a7d5a6cd12c1dd7e1b45bc8a4996ca3a58cad9c5
Brazil, Jim <jibrazil@populusgroup.com>
2018-11-01 15:42:23 -0400

    Refactored Create Subscriber

---
+++ Product/UpDiddyLib/Dto/SubscriberCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpDiddyLib.Dto
+{
+    public class SubscriberCreateDto : BaseDto
+    {
+        public string SubscriberGuid { get; set; }
+        public string SubscriberEmail { get; set; }
+    }
+}
