17114acf92bfb1444aa56734912d5425ad65804b
Ian Koplowitz <ikoplowitz@populusgroup.com>
2018-11-08 16:25:07 -0500

    changed back hangfire variable

--- Product/API/Startup.cs
+++ Product/API/Startup.cs
@@ -101,7 +101,7 @@ namespace UpDiddyApi
             services.AddAutoMapper(typeof(UpDiddyApi.Helpers.AutoMapperConfiguration));
 
             // Configure Hangfire 
-            var HangFireSqlConnection = Configuration["HangFireBrentDev"];
+            var HangFireSqlConnection = Configuration["CareerCircleSqlConnection"];
             services.AddHangfire(x => x.UseSqlServerStorage(HangFireSqlConnection));
             // Have the workflow monitor run every minute 
             JobStorage.Current = new SqlServerStorage(HangFireSqlConnection);
