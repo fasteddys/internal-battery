24cbf2bf0747140491bc6b4a0fec6ca759e8b774
Brazil, Jim <jibrazil@populusgroup.com>
2018-11-20 08:33:22 -0500

    Additional logging for CourseFactory

--- Product/API/Business/CourseFactory.cs
+++ Product/API/Business/CourseFactory.cs
@@ -71,8 +71,10 @@ namespace UpDiddyApi.Business
         #region Helper methods 
         private CourseLoginDto _GetWozCourseLogin(Guid SubscriberGuid, Guid CourseGuid, Guid VendorGuid)
         {
+            int step = 0; 
             try
             {
+               
                 _syslog.Log(LogLevel.Information, $"***** CourseFactory.GetWozCourseLogin started at: {DateTime.UtcNow.ToLongDateString()} for subscriber {SubscriberGuid} for course {CourseGuid} and vendor {VendorGuid}");
                 CourseLoginDto rVal = new CourseLoginDto();
                 rVal.LoginUrl = string.Empty;
@@ -84,39 +86,51 @@ namespace UpDiddyApi.Business
                             where s.SubscriberGuid == SubscriberGuid && vsl.VendorId == v.VendorId
                             select new { LastLoginDate = vsl.LastLoginDate, SubscriberGuid = s.SubscriberGuid, RegstrationUrl = vsl.RegistrationUrl, LoginUrl = v.LoginUrl };
 
+                step = 1;
                 if (studentLogin != null && studentLogin.First() != null)
                 {
                     DateTime? LastLoginDate = studentLogin.First().LastLoginDate;
                     Guid? subscriberGuid = studentLogin.First().SubscriberGuid;
+                    step = 2;
                     if (LastLoginDate == null)
                     {
-
+                        step = 3;
                         int HangFireDelay = int.Parse(_configuration["Woz:LastLoginUpdateDelayInMinutes"]);
+                        step = 4;
                         // Call hangfire to check to see if the user has logged into woz 
                         if (subscriberGuid != null)
                         {
+                            step = 5;
                             string cacheKey = $"CourseFactoryGetCourseLogin{subscriberGuid}";
                             string inProgressFlag = _cache.GetString(cacheKey);
                             // Only queue hangfire job is it has not been done recently
+                            step = 6;
                             if (string.IsNullOrEmpty(inProgressFlag))
                             {
+                                step = 7;
                                 BackgroundJob.Schedule<ScheduledJobs>(j => j.UpdateWozStudentLastLogin(subscriberGuid.ToString()), TimeSpan.FromMinutes(HangFireDelay));
                                 // The hangfire delay should be sufficient for the the cache TTL
                                 _cache.SetString(cacheKey, "Inprogress", new DistributedCacheEntryOptions() { AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(HangFireDelay) });
                             }
                         }
+                        step = 8;
                         rVal.LoginUrl = studentLogin.First().RegstrationUrl;
                     }
                     else
+                    {
+                        step = 9;
                         rVal.LoginUrl = studentLogin.First().LoginUrl;
+                    }
+                        
                 }
                 _syslog.Log(LogLevel.Information, $"***** CourseFactory.GetWozCourseLogin completed at: {DateTime.UtcNow.ToLongDateString()}");
+                step = 10;
                 return rVal;
             }
             catch (Exception ex)
             {
-                _syslog.Log(LogLevel.Error, "CourseFactory.GetWozCourseLogin threw an exception -> " + ex.Message);
-                _syslog.Log(LogLevel.Error, $"Paremeters subscriber= {SubscriberGuid}  course= {CourseGuid} vendor= {VendorGuid}");
+                _syslog.Log(LogLevel.Error, $"CourseFactory.GetWozCourseLogin: Exception at step {step} -> {ex.Message}");
+                _syslog.Log(LogLevel.Error, $"CourseFactory.GetWozCourseLogin: Paremeters subscriber= {SubscriberGuid}  course= {CourseGuid} vendor= {VendorGuid}");
                 return new CourseLoginDto()
                 {
                     LoginUrl = "Error in CourseFactory.GetWozCourseLogin"
