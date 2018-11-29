5c01a98dcadfc1d75fcb1e3137e8357b984b7815
Bill Koenig <wikoenig@populusgroup.com>
2018-11-29 11:00:37 -0500

    added comments for WebApp.Tests, corrected test project name in pipeline

--- Product/WebApp.Tests/LinkedInTests.cs
+++ Product/WebApp.Tests/LinkedInTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Rewrite;
+using Moq;
+using System;
+using UpDiddy.Helpers.RewriteRules;
 using Xunit;
 
 namespace WebApp.Tests
@@ -13,6 +17,21 @@ namespace WebApp.Tests
              * - how to mock 3rd party dependencies in our code (e.g. ADB2C, KeyVault, etc)?
              * - do we need to rewrite our classes/methods so that unit tests can be written for it?
              */
+
+            // arrange
+            var mockRewriteContext = new Mock<RewriteContext>();
+            HostString host = new HostString("careercircle.com");
+            // the following line generates this exception - System.NotSupportedException: 'Invalid setup on a non-virtual (overridable in VB) member: mock => mock.HttpContext'
+            //mockRewriteContext.SetupGet(x => x.HttpContext.Request.Host).Returns(host);
+
+            // todo: also need to mock Scheme, PathBase, Path, QueryString
+            //RedirectWwwRule redirectWwwRule = new RedirectWwwRule();
+
+            // act
+            //redirectWwwRule.ApplyRule(mockRewriteContext.Object);
+
+            // assert
+            // todo: inspect the values of response.StatusCode, response.Headers, context.Result
 
             // make happy noises
             Assert.True(true);
