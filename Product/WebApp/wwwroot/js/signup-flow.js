1d1156e792f1b06abf8ab4b0f4459f2849b863b3
Ian Koplowitz <ikoplowitz@populusgroup.com>
2018-12-14 16:58:51 -0500

    additional work on flow including adding cards to work history

--- Product/WebApp/wwwroot/js/signup-flow.js
+++ Product/WebApp/wwwroot/js/signup-flow.js
@@ -1,11 +1,50 @@
-﻿var workHistory = {};
+﻿var workHistory = [];
+class WorkHistoryItem {
+    constructor(_title, _org, _description) {
+        this.title = _title;
+        this.organization = _org;
+        this.description = _description;
+    }
+}
+
+var education = [];
+class Education {
+    constructor(_institution, _major) {
+        this.institution = _institution;
+        this.major = _major;
+    }
+}
 
 $(document).ready(function () {
     $(".add-work-history").on("click", function () {
         //$('.work-history-log').append('<div class="form-row"><div class="form-group col-md-6" ><input type="text" class="form-control" placeholder="Job Title"></div><div class="form-group col-md-6"><input type="text" class="form-control" placeholder="Organization"></div></div>');
     });
+    /*
+    $('#SignupFlowCarousel').bind('slide.bs.carousel', function (e) {
+        if ($('.carousel-item-second').hasClass('active')) {
+            $('.previous-container').removeClass("hidden");
+            $('.previous-container').removeClass("slideInUp");
+            $('.previous-container').addClass("slideOutDown");
+        }
+        else {
+            $('.previous-container').removeClass("slideOutDown");
+            $('.previous-container').addClass("slideInUp");
+        }
+    });*/
     $('.save-new-work-history-item').on("click", function () {
-        $(".work-history-log .row").append('<div class="col-12 col-sm-6 col-md-4"><div class="card"><div class= "card-body"><h5 class="card-title">Software Engineer</h5><h6 class="card-subtitle mb-2 text-muted">Allegis Group</h6><p class="card-text">Build an application in the .Net Core 2.1 framework.</p></div></div></div>');
+        var jobTitle = $('#JobTitleInput').val();
+        var organization = $('#OrganizationInput').val();
+        var description = $('#JobDescriptionInput').val();
+        if (jobTitle && organization && description) {
+            $('#WorkHistoryModal').modal("hide");
+            workHistory.push(new WorkHistoryItem(jobTitle, organization, description));
+            $(".work-history-log .row").append('<div class="col-12 col-sm-6 col-md-4"><div class="card"><div class= "card-body"><h5 class="card-title">' + jobTitle + '</h5><h6 class="card-subtitle mb-2 text-muted">' + organization + '</h6><p class="card-text">' + description + '</p></div></div></div>');
+            clearWorkHistoryModal();
+        }
+        else {
+            $('.work-history-modal-validation').show();
+        }
+        
     });
 
     $('#UploadedResume').change(function () {
@@ -51,3 +90,13 @@ $(document).ready(function () {
         });
     });
 });
+
+var clearWorkHistoryModal = function () {
+    $('#WorkHistoryModal').find("input").val("");
+    $('#WorkHistoryModal').find("textarea").val("");
+
+};
+
+var closeModals = function () {
+    $('.modal').modal("hide");
+};
