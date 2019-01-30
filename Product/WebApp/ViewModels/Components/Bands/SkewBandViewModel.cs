f8f99b01c3cb3a7ae2160478c80f3899423658be
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-01-30 18:55:20 -0500

    changed bands into partials

---
+++ Product/WebApp/ViewModels/Components/Bands/SkewBandViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace UpDiddy.ViewModels
+{
+    public class SkewBandViewModel : BandViewModel
+    {
+        public string SkewDirection { get; set; }
+        public string Header { get; set; }
+    }
+}
---
+++ Product/WebApp/ViewModels/Components/Bands/TextWithVideoBandViewModelcs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace UpDiddy.ViewModels
+{
+    public class TextWithVideoBandViewModel : BandViewModel
+    {
+        public string Header { get; set; }
+        public string DescriptionLine1 { get; set; }
+        public string DescriptionLine2 { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
