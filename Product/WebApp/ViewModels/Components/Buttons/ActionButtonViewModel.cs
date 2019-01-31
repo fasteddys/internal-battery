c47c41c6efa5faaa6dd5942820a0c57a5942b4e8
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-01-31 14:31:27 -0500

    added campaign band and added directory organization to scss files

---
+++ Product/WebApp/ViewModels/Components/Buttons/ActionButtonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace UpDiddy.ViewModels
+{
+    public class ActionButtonViewModel : BandViewModel
+    {
+        public string Text { get; set; }
+        public string SkewDirection { get; set; }
+    }
+}
