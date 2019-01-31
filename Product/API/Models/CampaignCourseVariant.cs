2b242c5c2b55c62e06cb67d5e147a5648652e5ae
Bill Koenig <wikoenig@populusgroup.com>
2019-01-31 10:41:31 -0500

    model changes to support displaying campaign information to user

---
+++ Product/API/Models/CampaignCourseVariant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpDiddyApi.Models
+{
+    public class CampaignCourseVariant : BaseModel
+    {
+        public int CampaignId { get; set; }
+        public virtual Campaign Campaign { get; set; }
+        public int CourseVariantId { get; set; }
+        public virtual CourseVariant CourseVariant { get; set; }
+        public Guid? CampaignCourseVariantGuid { get; set; }
+        public int? MaxRebateEligibilityInDays { get; set; }
+        public bool IsEligibleForRebate { get; set; }
+        public int RebateTypeId { get; set; }
+        public virtual RebateType RebateType { get; set; }
+        public int? RefundId { get; set; }
+        public virtual Refund Refund { get; set; } 
+    }
+}
