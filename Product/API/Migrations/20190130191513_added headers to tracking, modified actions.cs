483a42bacca0871e3f3ca7871ff5022ba872b523
Bill Koenig <wikoenig@populusgroup.com>
2019-01-31 11:12:23 -0500

    removed insert and update statements in migration related to Action (campaigns)

--- Product/API/Migrations/20190130191513_added headers to tracking, modified actions.cs
+++ Product/API/Migrations/20190130191513_added headers to tracking, modified actions.cs
@@ -11,20 +11,6 @@ namespace UpDiddyApi.Migrations
                 name: "Headers",
                 table: "ContactAction",
                 nullable: true);
-            
-            migrationBuilder.InsertData(
-                table: "Action",
-                columns: new[] { "IsDeleted", "ActionGuid", "CreateDate", "CreateGuid", "Name" },
-                values: new object[] { 0, Guid.Parse("CCBEE3A5-278E-4696-9CB6-AB6DC5B50D0A"), DateTime.UtcNow, Guid.Empty, "Complete course" }
-                );
-
-            migrationBuilder.UpdateData(
-                table: "Action",
-                keyColumn: "ActionGuid",
-                keyValue: Guid.Parse("47D62280-213F-44F3-8085-A83BB2A5BBE3"),
-                column: "Name",
-                value: "Visit landing page"
-                );
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
