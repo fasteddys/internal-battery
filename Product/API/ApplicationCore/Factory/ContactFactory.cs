ef007eee8a522fe505e0fd72b651fc50728edffd
Bill Koenig <wikoenig@populusgroup.com>
2019-02-20 18:12:08 -0500

    work in progress for contact ui

--- Product/API/ApplicationCore/Factory/ContactFactory.cs
+++ Product/API/ApplicationCore/Factory/ContactFactory.cs
@@ -8,7 +8,38 @@ namespace UpDiddyApi.ApplicationCore.Factory
 {
     public class ContactFactory
     {
+        static public Contact CreateContact(string email, string firstName, string lastName, Subscriber subscriber)
+        {
+            Contact rVal = new Contact();
+            rVal.Email = email;
+            rVal.FirstName = firstName;
+            rVal.LastName = lastName;
+            if (subscriber != null && subscriber.SubscriberId > 0)
+                rVal.SubscriberId = subscriber.SubscriberId;
+            rVal.CreateDate = DateTime.UtcNow;
+            rVal.CreateGuid = Guid.Empty;
+            rVal.ModifyDate = DateTime.UtcNow;
+            rVal.ModifyGuid = Guid.Empty;
+            rVal.ContactGuid = Guid.NewGuid();
+            rVal.IsDeleted = 0;
+            return rVal;
+        }
 
+        static public Contact GetOrAdd(UpDiddyDbContext db, string email, string firstName, string lastName, Subscriber subscriber)
+        {
+            email = email.Trim();
+            Contact contact = db.Contact
+                .Where(c => c.IsDeleted == 0 && c.Email == email)
+                .FirstOrDefault();
+
+            if (contact == null)
+            {
+                contact = CreateContact(email, firstName, lastName, subscriber);
+                db.Contact.Add(contact);
+                db.SaveChanges();
+            }
+            return contact;
+        }
 
         public static Contact GetContactByEmail(UpDiddyDbContext db, string email)
         {
--- Product/WebApp/Controllers/AdminController.cs
+++ Product/WebApp/Controllers/AdminController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UpDiddy.Api;
 using UpDiddyLib.Dto;
@@ -84,6 +88,53 @@ namespace UpDiddy.Controllers
             // todo: exception handling
             _api.UpdateEntitySkills(entitySkillDto);
             return Ok();
+        }
+
+        [HttpGet]
+        [Route("/admin/contacts")]
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("/admin/uploadcontacts")]
+        public IActionResult UploadContacts(IFormFile contactsFile)
+        {
+            IEnumerable<ContactDto> contacts;
+            using (var reader = new StreamReader(contactsFile.OpenReadStream()))
+            {
+                using (var csv = new CsvReader(reader))
+                {
+                    // use this? csv.Configuration.PrepareHeaderForMatch
+                    csv.Configuration.RegisterClassMap<ContactDtoMap>();
+                    contacts = csv.GetRecords<ContactDto>();
+                }
+            }
+
+            if (contacts != null)
+            {
+                // validation
+            }
+
+            return new JsonResult(contacts);
+        }
+        
+        public sealed class ContactDtoMap : ClassMap<ContactDto>
+        {
+            public ContactDtoMap()
+            {
+                Map(c => c.Email).Name("email");
+                Map(c => c.FirstName).Name("first_name");
+                Map(c => c.LastName).Name("last_name");
+                Map(c => c.ContactGuid).Ignore();
+                Map(c => c.CreateDate).Ignore();
+                Map(c => c.CreateGuid).Ignore();
+                Map(c => c.IsDeleted).Ignore();
+                Map(c => c.ModifyDate).Ignore();
+                Map(c => c.ModifyGuid).Ignore();
+            }
         }
     }
 }
