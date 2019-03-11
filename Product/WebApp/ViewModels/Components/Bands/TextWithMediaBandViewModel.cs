96aeb984fbab7881815e973c68b2130e8e1bf0f0
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-03-11 16:59:04 -0400

    Added new community landing page

--- Product/WebApp/ViewModels/Components/Bands/TextWithMediaBandViewModel.cs
+++ Product/WebApp/ViewModels/Components/Bands/TextWithMediaBandViewModel.cs
@@ -6,12 +6,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UpDiddy.ViewModels
 {
-    public class TextWithVideoBandViewModel : ComponentViewModel
+    public class TextWithMediaBandViewModel : ComponentViewModel
     {
         public string Header { get; set; }
-        public string VideoUrl { get; set; }
+        public string MediaUrl { get; set; }
         public string VideoThumbnailUrl { get; set; }
         public List<String> Descriptions { get; set; }
         public bool SwitchOrder { get; set; }
+        public enum MediaType { IMAGE, VIDEO};
+        public MediaType Media { get; set; }
+        public enum Breakpoint { TABLET, MOBILE}
+        public Breakpoint ChangeOn { get; set; }
+        public bool IncludeCTA { get; set; }
     }
 }
--- Product/WebApp/client/js/core/components/buttons/_action.js
+++ Product/WebApp/client/js/core/components/buttons/_action.js
@@ -1,4 +1,4 @@
-﻿$("#ButtonComponent").on("click", function (e) {
+﻿$(".button-container").on("click", function (e) {
     if ($(this).data("action-type") === "SUBMIT") {
         e.preventDefault();
         var form = $(this).data("form");
