0c91c129571f1efa114169fc8d712cf41808c472
Chris McGee <christmcgee@populusgroup.com>
2019-03-19 23:20:31 -0400

    hotfix: small adjustments in styling and content for landing pages

--- Product/WebApp/Controllers/CampaignController.cs
+++ Product/WebApp/Controllers/CampaignController.cs
@@ -40,7 +40,7 @@ namespace UpDiddy.Controllers
         }
 
         [HttpGet]
-        [Route("/Community/{CampaignGuid?}/{ContactGuid?}")]
+        [Route("/Join/{CampaignGuid?}/{ContactGuid?}")]
         public async Task<IActionResult> CampaignAsync(Guid CampaignGuid, Guid ContactGuid)
         {
             if (CampaignGuid == Guid.Empty || ContactGuid == Guid.Empty)
