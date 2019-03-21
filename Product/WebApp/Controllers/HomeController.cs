43dd266f5b766c98c35203375013d57d37a35a11
Chris McGee <christmcgee@populusgroup.com>
2019-03-21 09:03:42 -0400

    hotfix: ios fix for express sign up

--- Product/WebApp/Controllers/HomeController.cs
+++ Product/WebApp/Controllers/HomeController.cs
@@ -805,7 +805,7 @@ namespace UpDiddy.Controllers
                         return Ok(new BasicResponseDto
                         {
                             StatusCode = subscriberResponse.StatusCode,
-                            Description = "/Home/Signup"
+                            Description = "/Home/LoggingIn"
                         });
                     default:
                         // If there's an error from contact-to-subscriber converstion API call,
