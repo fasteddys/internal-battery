d72edb98bd80240f4b946e061ccc717a12583c33
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-03-25 16:38:21 -0400

    fixed csproj and added virtual link in offerDto to partnerDto

--- Product/UpDiddyLib/Dto/OfferDto.cs
+++ Product/UpDiddyLib/Dto/OfferDto.cs
@@ -9,6 +9,7 @@ namespace UpDiddyLib.Dto
         public int OfferId { get; set; }
         public Guid OfferGuid { get; set; }
         public int PartnerId { get; set; }
+        public virtual PartnerDto Partner { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Disclaimer { get; set; }
