9715ca2f67ce5e061e1728be9215563be806861b
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-03-25 16:39:51 -0400

    removed partnerDto virtual reference from offerDto

--- Product/UpDiddyLib/Dto/OfferDto.cs
+++ Product/UpDiddyLib/Dto/OfferDto.cs
@@ -9,7 +9,6 @@ namespace UpDiddyLib.Dto
         public int OfferId { get; set; }
         public Guid OfferGuid { get; set; }
         public int PartnerId { get; set; }
-        public virtual PartnerDto Partner { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Disclaimer { get; set; }
