e5d91e2a15691b6139124d2a0b3c5cb6850c386d
Chris McGee <christmcgee@populusgroup.com>
2019-04-01 14:08:51 -0400

    task-411: Initial integration work with Azure API Management

---
+++ Product/API/ApplicationCore/Interfaces/IAPIGateway.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpDiddyApi.ApplicationCore.Services.AzureAPIManagement;
+
+namespace UpDiddyApi.ApplicationCore.Interfaces
+{
+    public interface IAPIGateway
+    {
+        Task<string> GetUserIdAsync(string subscriptionId, string key);
+        Task<User> GetUserAsync(string userId);
+    }
+}
---
+++ Product/API/Authorization/APIGateway/APIGatewayDefaults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpDiddyApi.Authorization.APIGateway
+{
+    public static class APIGatewayDefaults
+    {
+        public const string AuthenticationScheme = "APIGateway Scheme";
+        public const string DisplayName = "APIGateway Auth";
+    }
+}
