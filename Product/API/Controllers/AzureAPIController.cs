86e4d8f7d54b9aed73b5af7bbc1e3da7835b10a7
Chris McGee <christmcgee@populusgroup.com>
2019-04-01 14:47:37 -0400

    task-411: Added test endpoint for API Management debugging and troubleshooting

---
+++ Product/API/Controllers/AzureAPIController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UpDiddyApi.Authorization.APIGateway;
+
+namespace UpDiddyApi.Controllers
+{
+    public class AzureAPIController : Controller
+    {
+        // GET: /api/azure-api/me
+        [HttpGet("/api/azure-api/me")]
+        [Authorize(AuthenticationSchemes = APIGatewayDefaults.AuthenticationScheme)]
+        public IActionResult MyAccount()
+        {
+            string userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            string email = HttpContext.User.FindFirst(ClaimTypes.Email).Value;
+            string name = HttpContext.User.FindFirst(ClaimTypes.Name).Value;
+            return Json(new { userId, email, name, Message = "Success" });
+        }
+    }
+}
