c386a7a6afcfccf557f7fc78a5891a84402d7f84
Chris McGee <christmcgee@populusgroup.com>
2019-04-08 12:55:17 -0400

    hotfix: video now fades in

--- Product/WebApp/client/js/core/site.js
+++ Product/WebApp/client/js/core/site.js
@@ -208,4 +208,17 @@
                 $phone.val("");
         });
     
+
+    // video section
+    $('.video-section video').each(function(index, ele) {
+        if(ele.readyState > 3) {
+            $(ele).addClass('can-play');
+            return;
+        }
+
+        ele.oncanplay = function() {
+            $(this).addClass('can-play');
+        }
+
+    });
 });
