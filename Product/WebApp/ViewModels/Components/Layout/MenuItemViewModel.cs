3e14603d99cdfe0b0658309909cc5c0e1aa04a3a
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-04-10 13:22:54 -0400

    continued work on integration with butter. Stuck on if menu item is get path instead of menu item object

---
+++ Product/WebApp/ViewModels/Components/Layout/MenuItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpDiddy.ViewModels.Components.Layout
+{
+    public class MenuItemViewModel
+    {
+        public string url { get; set; }
+        public string label { get; set; }
+        public bool isroot { get; set; }
+        public IList<MenuItemViewModel> child_pages { get; set; }
+    }
+}
