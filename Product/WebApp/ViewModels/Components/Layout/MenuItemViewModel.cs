60da4ed7ccbf9414342e0adc1ec133ba6538018d
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-04-10 19:58:05 -0400

    added logic to handle only showing some links after authentication

--- Product/WebApp/ViewModels/Components/Layout/MenuItemViewModel.cs
+++ Product/WebApp/ViewModels/Components/Layout/MenuItemViewModel.cs
@@ -18,5 +18,8 @@ namespace UpDiddy.ViewModels.Components.Layout
         public bool NeedsAdminPermissions { get; set; }
         [JsonProperty("needs_talent_permissions")]
         public bool NeedsTalentPermissions { get; set; }
+        [JsonProperty("needs_authentication")]
+        public bool NeedsAuthentication { get; set; }
+
     }
 }
