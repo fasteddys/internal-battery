e559a9ae1bf6a4e314b1898be1ebd3d3eb6446b9
Ian Koplowitz <ikoplowitz@populusgroup.com>
2019-04-16 17:13:15 -0400

    initial commit

--- Product/WebApp/Controllers/TopicsController.cs
+++ Product/WebApp/Controllers/TopicsController.cs
@@ -3,24 +3,38 @@ using Microsoft.Extensions.Configuration;
 using UpDiddy.Api;
 using UpDiddyLib.Dto;
 using UpDiddy.ViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace UpDiddy.Controllers
 {
-  
-    public class TopicController : BaseController
+
+    [Route("[controller]")]
+    public class TopicsController : BaseController
     {
         private readonly IConfiguration _configuration;       
 
-        public TopicController(IApi api, IConfiguration configuration)
+        public TopicsController(IApi api, IConfiguration configuration)
              : base(api)
         {
             _configuration = configuration;
         }
 
         [HttpGet]
-        [Route("/Topic/{TopicSlug}")]
+        public async Task<IActionResult> IndexAsync()
+        {
+            // TODO: We don't have the Partners linked to the courses that they offer... 
+            TopicsViewModel TopicsViewModel = new TopicsViewModel(_configuration)
+            {
+                Topics = await _Api.TopicsAsync()
+            };
+
+            return View("Index", TopicsViewModel);
+        }
+
+        [HttpGet("{TopicSlug}")]
         public async System.Threading.Tasks.Task<IActionResult> GetAsync(string TopicSlug)
         {         
             TopicDto Topic = await _Api.TopicBySlugAsync(TopicSlug);
@@ -28,11 +42,6 @@ namespace UpDiddy.Controllers
         
             return View("Details", TopicViewModel);
         }
-
-        // GET: /<controller>/
-        public IActionResult Index()
-        {               
-            return View();
-        }
+        
     }
 }
---
+++ Product/WebApp/ViewModels/TopicsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using UpDiddyLib.Dto;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UpDiddy.ViewModels
+{
+    public class TopicsViewModel : BaseViewModel
+    {
+        public IList<TopicDto> Topics { get; set; }
+        public TopicsViewModel(IConfiguration _configuration)
+        {
+            this.ImageUrl = _configuration["BaseImageUrl"];
+        }
+        
+    }
+}
