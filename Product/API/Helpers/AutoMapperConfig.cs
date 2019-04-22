84fdfae1760195d3c88309a41884dc71f96df548
Chris McGee <christmcgee@populusgroup.com>
2019-04-22 02:14:01 -0400

    task-538: wrapped up resume refactor to react poc w/ssr

--- Product/API/Helpers/AutoMapperConfig.cs
+++ Product/API/Helpers/AutoMapperConfig.cs
@@ -38,7 +38,6 @@ namespace UpDiddyApi.Helpers
             CreateMap<EnrollmentLog, EnrollmentLogDto>().ReverseMap();
             CreateMap<CourseVariantType, CourseVariantTypeDto>().ReverseMap();
             CreateMap<Skill, SkillDto>().ReverseMap();
-            CreateMap<SubscriberFile, SubscriberFileDto>().ReverseMap();
             CreateMap<Company, CompanyDto>().ReverseMap();
             CreateMap<EducationalInstitution, EducationalInstitutionDto>().ReverseMap();
             CreateMap<EducationalDegree, EducationalDegreeDto>().ReverseMap();
@@ -132,6 +131,12 @@ namespace UpDiddyApi.Helpers
                 .ForMember(s => s.WorkHistory, opt => opt.MapFrom(src => src.SubscriberWorkHistory))
                 .ForMember(s => s.EducationHistory, opt => opt.MapFrom(src => src.SubscriberEducationHistory))
                 .ForMember(s => s.Files, opt => opt.MapFrom(src => src.SubscriberFile))
+                .ReverseMap();
+            CreateMap<SubscriberFile, SubscriberFileDto>()
+                .ForMember(s => s.SimpleName, opt => opt.MapFrom(src => src.SimpleName))
+                .ForMember(s => s.SubscriberFileGuid, opt => opt.MapFrom(src => src.SubscriberFileGuid))
+                .ForMember(s => s.CreateDate, opt => opt.MapFrom(src => src.CreateDate))
+                .ForMember(s => s.BlobName, opt => opt.MapFrom(src => src.BlobName))
                 .ReverseMap();
             CreateMap<State, StateDto>()
                 .ForMember(s => s.Country, opt => opt.MapFrom(src => src.Country))
--- Product/WebApp/client/react/components/ResumeUpload.js
+++ Product/WebApp/client/react/components/ResumeUpload.js
@@ -3,21 +3,115 @@ import React from 'react';
 class ResumeUpload extends React.Component {
     constructor(props) {
         super(props)
+        this.fileInput = React.createRef();
         this.state = {
             fileName: props.fileName,
             fileGuid: props.fileGuid,
-            mode: props.fileGuid ? "view" : "new"
+            processing: false,
+            mode: props.fileGuid ? 1 : 0,
+            selectedFile: null
         };
     }
 
-    render() {
+    onFileSelect(files) {
+        this.setState({selectedFile: files[0]});
+    }
+
+    hasResume() {
+        return this.state.fileGuid ? true : false;
+    }
+
+    changeMode(mode){
+        this.setState({mode, selectedFile: null});
+    }
+
+    uploadResume(e) {
+        e.preventDefault();
+        if(!this.state.selectedFile)
+            return;
+
+        this.setState({processing: true}, () => {
+            CareerCircleAPI.uploadResume(this.state.selectedFile, false)
+                .then((response) => {
+                    this.setState({mode: 1, fileName: response.data.simpleName, fileGuid: response.data.subscriberFileGuid});
+                })
+                .catch((err) => console.log(err))
+                .finally(() => {
+                    this.setState({processing: false});
+                });
+        });
+
+    }
+
+    deleteResume() {
+        this.setState({processing: true}, () => {
+            CareerCircleAPI.deleteFile(this.state.fileGuid)
+                .then((response) => {
+                    this.setState({mode: 0, fileName: null, fileGuid: null, selectedFile: null});
+                })
+                .catch((err) => console.log(err))
+                .finally(() => {
+                    this.setState({processing: false});
+                });
+        });
+    }
+
+    spinner() {
+        if(!this.state.processing)
+            return;
+
+        return  (<div className="d-inline">
+            <span className="loading-spinner spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            <span className="loading-spinner sr-only">Loading...</span>
+        </div>);
+    }
+
+    viewMode() {
         return (
             <div>
-                <a className="download" href="/Home/DownloadFile?fileGuid={this.state.fileGuid}" target="_blank"><i className="fas fa-download" title="Download"></i> { this.state.fileName }</a>
-                <button className="edit"><i className="fas fa-edit text-light-blue" title="Edit"></i></button>
-                <button className="delete"><i className="fas fa-minus-circle text-light-blue" title="Delete"></i></button>
+                <a className="text-primary download-link pr-1" href={`/Home/DownloadFile?fileGuid=${this.state.fileGuid}`} target="_blank"><i className="fas fa-download" title="Download"></i> { this.state.fileName }</a>
+                <button type="button" className="btn btn-text text-primary edit px-2" onClick={() => this.changeMode(0)}><i className="fas fa-edit text-light-blue" title="Edit"></i></button>
+                <button type="button" className="btn btn-text text-primary delete px-2" onClick={() => this.deleteResume()}><i className="fas fa-minus-circle text-light-blue" title="Delete"></i> { this.spinner() }</button>
             </div>
         );
+    }
+
+    uploadMode() {
+        let cancelBtn;
+        
+        if(this.state.fileGuid != null)
+            cancelBtn = <button type="button" onClick={() => this.changeMode(1)} className="btn btn-sm btn-secondary cancel">Cancel</button>;
+        return (
+            <form className="" method="post" action="/Home/UploadResume" encType="multipart/form-data">
+                <div>
+                    { this.state.selectedFile ? (
+                    <div className="d-inline-block">
+                        <label htmlFor="react-upload" className="pr-1">
+                            <span className="file-name text-primary">{ this.state.selectedFile.name }</span>&nbsp;<i className="fas fa-exchange-alt text-primary"></i>
+                        </label>
+                        <button type="submit" onClick={(e) => this.uploadResume(e)} className="submit btn btn-sm btn-light-blue mr-2">
+                            Save { this.spinner() }
+                        </button>
+                    </div>
+                    )
+                    :
+                    (
+                        <label htmlFor="react-upload" className="btn btn-sm btn-light-blue mb-0 mr-2">
+                            <i className="fas fa-file-upload"></i>&nbsp;Upload Resume
+                        </label>
+                    )}
+
+                    { cancelBtn }
+                    <input required id="react-upload" className="pseudo-hidden file-input" type="file" onChange={(e) => this.onFileSelect(e.target.files)} accept=".doc,.docx,.odt,.pdf,.rtf,.tex,.txt,.wks,.wps,.wpd" />
+                </div>
+            </form>
+        );
+    }
+
+    render() {
+        return (<div className="resume-upload-component">
+            { this.state.mode == 0 ? this.uploadMode() : this.viewMode()}
+        </div>);
     }
 }
 
