870cca13b8b627588920f96a9b39ca032caaf74d
ikoplowitz <ikoplowitz@populusgroup.com>
2019-05-14 13:05:42 -0400

    added authorization handler to view hangfire dashboard in staging

---
+++ Product/API/Authorization/HangfireAuthorizationFilter.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpDiddyApi.Authorization
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        private IHostingEnvironment Environment;
+        public HangfireAuthorizationFilter(IHostingEnvironment _Environment)
+        {
+            Environment = _Environment;
+        }
+        public bool Authorize(DashboardContext context)
+        {
+            if (Environment.IsDevelopment() || Environment.IsStaging())
+                return true;
+            return false;
+        }
+    }
+}
--- Product/API/Startup.cs
+++ Product/API/Startup.cs
@@ -279,7 +279,10 @@ namespace UpDiddyApi
 
             app.UseCors("Cors");
 
-            app.UseHangfireDashboard("/dashboard");
+            app.UseHangfireDashboard("/dashboard", new DashboardOptions
+            {
+                Authorization = new[] { new HangfireAuthorizationFilter(env) }
+            });
             app.UseHangfireServer();
 
             app.UseMvc(routes =>
