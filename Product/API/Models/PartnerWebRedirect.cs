49f809a2361643c594ea670da8f31d6858a5c6f7
Chris McGee <christmcgee@populusgroup.com>
2019-06-03 10:33:18 -0400

    story-757: signup modal and onboarding custom redirect added

---
+++ Product/API/Models/PartnerWebRedirect.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpDiddyApi.Models
+{
+    public class PartnerWebRedirect
+    {
+        [Key]
+        [JsonIgnore]
+        public int PartnerId { get; set; }
+        public string RelativePath { get; set; }
+    }
+}
