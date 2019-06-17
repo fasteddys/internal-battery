16eb7c54078718036a34025fd6266731a321a38b
Brazil, Jim <jibrazil@populusgroup.com>
2019-06-17 17:57:57 -0400

    06-17-19

--- Product/WebApp/client/react/components/ResumeMerge.js
+++ Product/WebApp/client/react/components/ResumeMerge.js
@@ -1,5 +1,18 @@
 ﻿import React from 'react';
  
+ 
+
+// Local componenet for rendering skill inputs 
+const SkillCheckbox = ({resultGuid, parsedValue, checked, onChange  }) => (
+        <input type="checkbox" name={"chk_" + resultGuid} id={"chk_" + resultGuid} value={parsedValue} checked={checked}    onChange={(e) => onChange(e)} />  
+);
+
+
+const  SkillScrollableDivStyle = { maxHeight: "300px", overflow : "scroll", position : "relative" }
+const SkillScrollMoreStyle = {
+    textAlign: "center", backgroundColor: "#007bff", opacity: "0.9", color: "white", borderRadius: "3"}
+
+
 
 class ResumeMerge extends React.Component {
  
@@ -8,13 +21,19 @@ class ResumeMerge extends React.Component {
         this.state = { 
             processing: false,
             questions: null,
-            hasQuestions: false
+            hasQuestions: false,
+            selectAllSkills : false,
+            skillChecks : null,
+            selectAllSkillsLabel: "Select All",
+            displayScrollMessage : true  
         };
-        this.onDoParseMerge = this.onDoParseMerge.bind(this);    
+        this.onDoParseMerge = this.onDoParseMerge.bind(this);      
+        this.onSkillChange = this.onSkillChange.bind(this);   
+       
 }
 
 
-
+ 
     spinner() { 
 
         if (!this.state.processing)
@@ -27,14 +46,72 @@ class ResumeMerge extends React.Component {
     }
 
 
-    onDoParseMerge(e) {
-        console.log("onDoParseMerge detail = " + e.detail);            
+
+    selectAllSkills()
+    {        
+ 
+       if ( this.state.selectAllSkills  == false  )
+            this.setState({ selectAllSkillsLabel: "Select All" });
+       else 
+            this.setState({ selectAllSkillsLabel: "Unselect All" });
+
+        let skillChecks = this.state.skillChecks;
+
+        skillChecks.forEach(q => {        
+          q.checked = this.state.selectAllSkills;
+        })
+        this.forceUpdate();
+  
+
+    }
+
+    onSelectAllSkills()
+    { 
+       this.setState({selectAllSkills: !this.state.selectAllSkills},
+       this.selectAllSkills) 
+    }
+ 
+    onScrollSkills(e)
+    {
+        var ctl = $("#divSkills")[0];  
+        if (ctl.scrollHeight <= $("#divSkills").height()) {
+            this.setState({ displayScrollMessage: false });
+        }
+        else {
+            var showScrollMessage = (ctl.scrollTop + $("#divSkills").height()) != ctl.scrollHeight;
+            this.setState({ displayScrollMessage: showScrollMessage });
+            // show the scroll message here since OnComponentUpdate will fade it out
+            $("#divScrollMessage").show();
+        }
+ 
+    }
+
+    onDoParseMerge(e) {      
         CareerCircleAPI.getResumeParseMergeQuestionnaire(e.detail)
             .then((response) => {
                 this.setState({ questions: response });
                 this.setState({ hasQuestions: true });
+
+                let skillChecks = response.data.skills.map((o, index) =>
+                (
+                   {
+                       resultGuid:  o.resumeParseResultGuid,
+                       parsedValue: o.parsedValue,
+                       checked: false,
+                       existingValue: o.existingValue                                     
+                   }
+                     
+                ))
+
+                this.setState({ skillChecks: skillChecks });
                 $("#ResumeMergeModal").modal();
-                //alert(this.state.questions.data.resumeParseGuid )
+
+                // hide the scroll message for 6 or less skills since
+                // they will show without scrolling
+                if (skillChecks != null && skillChecks.length <= 6)
+                    this.setState({ displayScrollMessage: false });
+
+             
          
             })
             .catch((err) => {
@@ -69,58 +146,67 @@ class ResumeMerge extends React.Component {
     }
 
 
-    formatSkillsGood(skills) {
-        let rval = <div className="profile-edit-modal-header-container">
+   formatSkillsScrollMessage()
+   {
+       return this.state.displayScrollMessage ?  <div className="p-1 rounded" style={SkillScrollMoreStyle} id="divScrollMessage" > Scroll for more skills... </div>  : null
+   }
+
+
+    onSkillChange(e)
+    { 
+        let skillChecks = this.state.skillChecks
+        skillChecks.forEach(q => {
+        if (q.existingValue === event.target.value)
+            q.checked =  event.target.checked
+        })
+        this.setState({skillChecks: skillChecks})
+    }
+
+
+    formatSkills(skills) {
+
+        let rval =<div> 
+            <div className="profile-edit-modal-header-container" style={SkillScrollableDivStyle} id="divSkills"  onScroll={(e) => this.onScrollSkills(e)}  >
             <h5> Which of the following skills do you have? </h5>
-            <ul className="list-group">
+            <div className = "p-2">     <input type="checkbox" name="chkSelectAllParsedSkills" id="chkSelectAllParsedSkills" onClick={(e) => this.onSelectAllSkills(e)} />  {this.state.selectAllSkillsLabel}  </div>
+            <ul className="list-group ">
                 {
-                    skills.map((o, index) =>
+                    this.state.skillChecks.map((o, index) =>
                         (
-                            <li className="list-group-item border-0 p-0 " key={o.resumeParseResultGuid}>
+                            <li className="list-group-item border-0 p-0 " key={o.resultGuid}>
                                 <div className="ml-3 h6">
-                                    <input type="checkbox" name={"chk_" + o.resumeParseResultGuid} id={"chk_" + o.resumeParseResultGuid} value={o.parsedValue} />  {o.existingValue}
+
+                                <SkillCheckbox resultGuid={o.resultGuid}  parsedValue={o.parsedValue} existingValue={o.existingValue} checked={o.checked} onChange={this.onSkillChange} />             <label>     {o.existingValue}   </label>         
+
                                 </div>
                             </li>
                         ))
                 }
-            </ul>
+            </ul>           
+        </div>       
+        { this.formatSkillsScrollMessage() } 
         </div>
         return rval;
     }
-
-    formatSkills(skills) {
-        let rval = <div className="profile-edit-modal-header-container">
-            <h5> Which of the following skills do you have? </h5>
-            <div className="">
-            
-                {
-                    skills.map((o, index) =>
-                        (
-                            <span className="ml-3 h6" style={{ }} key={o.resumeParseResultGuid}>
-                                <label>   <input type="checkbox" name={"chk_" + o.resumeParseResultGuid} id={"chk_" + o.resumeParseResultGuid} value={o.parsedValue} />  {o.existingValue}  </label>
-                                </span>                            
-                        ))
-                    }
-           
-            </div>
-        </div>
-        return rval;
-    }
-
-
  
-
-
-
-     
     displayWait() {
         this.setState({ processing: true })
     }
 
     componentDidMount() {
-        window.addEventListener('onDoParseMerge', this.onDoParseMerge);
-        console.log("--------------------------------- Did Mount ----------------------------------   ")
+        window.addEventListener('onDoParseMerge', this.onDoParseMerge);   
     }
+
+    componentDidUpdate() {
+        window.addEventListener('onDoParseMerge', this.onDoParseMerge);
+        // fade the message our here since it will always be initally shown, even when
+        // there are not enough skill to warrant scrolling 
+        $("#divScrollMessage").fadeOut(5000);
+ 
+
+
+    }
+
 
     render() {
  
@@ -142,10 +228,8 @@ class ResumeMerge extends React.Component {
         if (this.state.hasQuestions == true && this.state.questions.data.educationHistoryQuestions.length > 0)
             educationHistoryQuestions = this.formatQuestions(this.state.questions.data.educationHistoryQuestions);
 
-        if (this.state.hasQuestions == true && this.state.questions.data.skills.length > 0)
+        if (this.state.hasQuestions == true && this.state.questions.data.skills.length > 0 &&  this.state.skillChecks != null)
             skillQuestions = this.formatSkills(this.state.questions.data.skills);
-
- 
   
         return (
             <form action={"/Home/resume-merge/" + parseGuid}  method="post" id="ResumeMergeForm">   
