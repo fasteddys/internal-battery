e75018e4ece0cad304239f2724718312954de87d
Brazil, Jim <jibrazil@populusgroup.com>
2019-06-20 17:06:38 -0400

    06-20-19

--- Product/CCProfileService/server.js
+++ Product/CCProfileService/server.js
@@ -9,18 +9,84 @@ var http = require('http');
 var port = process.env.PORT;
 
 const app = express() 
+
+
+/**
+ * Create a Tenant.
+ */
+const createTenant = async (
+    tenantServiceClient,
+    parent,
+    tenantToBeCreated
+) => {
+    try {
+
+        const request = {
+            parent: parent,
+            tenant: tenantToBeCreated,
+        };
+
+        const tenantCreated = await tenantServiceClient.createTenant(request)
+            .then((x) => {
+
+                console.log(`Tenant created: ${JSON.stringify(tenantCreated[0])}`);
+            });
+
+      
+        return tenantCreated[0];
+    } catch (e) {
+        console.error(`Got exception while creating tenant!`);
+        throw e;
+    }
+};
+
+
+function test() {
+
+    throw ("test exception ");
+
+}
+
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.listen(port)
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.send('hello world');
 })
 
 
-app.get('/jimbo', function (req, res) {
-    res.send('hello jimbo')
+app.get('/:name', function (req, res) {
+    res.send( 'hello ' + req.params.name )
 })
+
+
+app.get('/create-tenant/:tenantName', function (req, res) {
+
+    let tenantServiceClient = new talentAPI.TenantServiceClient({
+        projectId: process.env.ProjectId,
+        keyFilename: process.env.KeyFilePath,
+    });
+
+
+    try {   
+        test();
+        createTenant(talentAPI.tenantServiceClient, process.env.ProjectName, 'CareerCircleProfiles-Dev')
+        res.send('Tenant ' + req.params.tenantName + ' has been created for ' + process.env.projectId);
+    }
+    catch (ex) {
+        res.send('Error creating tenant:' + ex);
+    }
+
+  
+})
+
+
+
+
+
 
 
 
