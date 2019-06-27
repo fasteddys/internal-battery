e3aca36ce9eadb9aff3b503d3a1446648cadb920
Chris McGee <christmcgee@populusgroup.com>
2019-06-27 13:55:35 -0400

    devops: added configurable tag option to test slots

--- Product/API/Controllers/VersionController.cs
+++ Product/API/Controllers/VersionController.cs
@@ -1,14 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace UpDiddyApi.Controllers
 {
     public class VersionController : Controller
     {
+        private string _tag { get; set; }
+
+        public VersionController(IConfiguration configuration)
+        {
+            _tag = configuration["Version:Tag"];
+        }
         [HttpGet]
         [Route("")]
         public IActionResult Get()
         {
             return Content($"Version: {typeof(Startup).Assembly.GetName().Version.ToString()}");
+        }
+
+        [HttpGet]
+        [Route("[controller]/tag")]
+        public IActionResult Tag()
+        {
+            return Content(_tag);
         }
     }
 }
