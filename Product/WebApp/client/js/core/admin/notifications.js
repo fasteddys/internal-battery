325821fc0143d02e2e749557ddd88a0de3f49d16
Brazil, Jim <jibrazil@populusgroup.com>
2019-07-25 12:59:26 -0400

    ready for final dev pull

--- Product/WebApp/client/js/core/admin/notifications.js
+++ Product/WebApp/client/js/core/admin/notifications.js
@@ -1,14 +1,7 @@
 ﻿
 
 
-
-$("#ModifyNotificationForm").on("submit", function (e) {
-
-    if (!$(this).find("#Title").val() || !$(this).find("#Description").val()) {
-        e.preventDefault();
-        ToastService.error('Please enter information for both Title and Description. To delete, return to Partners list and delete partner.', 'Oops, Something went wrong.');
-    }
-});
+ 
 
 $("#DeleteNotificationConfirmationModal .btn-danger").on("click", function () {
     window.location.replace("/admin/deletenotification/" + $("#NotificationToDelete").val());
