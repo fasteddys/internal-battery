ccc46eb543c7b7592e3956f6339d601560d433cc
Jyoti Guin <jguin@allegisgroup.com>
2019-08-15 14:42:50 -0400

    commit

--- Product/WebApp/Controllers/HomeController.cs
+++ Product/WebApp/Controllers/HomeController.cs
@@ -59,7 +59,7 @@ namespace UpDiddy.Controllers
         {
             try
             {
-                HomeViewModel HomeViewModel = new HomeViewModel(_configuration, await _Api.TopicsAsync());
+                HomeViewModel HomeViewModel = new HomeViewModel(_configuration);
                 var jobCount = await _Api.GetJobCountPerProvinceAsync();
                 HomeViewModel.JobCount = jobCount;
                 return View(HomeViewModel);
