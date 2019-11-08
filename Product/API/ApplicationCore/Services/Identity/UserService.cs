c52a0130a69a06950d5f231426e8cd5405be3b55
Bill Koenig <wikoenig@populusgroup.com>
2019-11-08 14:26:00 -0500

    additional bug fixes for Auth0

--- Product/API/ApplicationCore/Services/Identity/UserService.cs
+++ Product/API/ApplicationCore/Services/Identity/UserService.cs
@@ -186,9 +186,17 @@ namespace UpDiddyApi.ApplicationCore.Services.Identity
         public async Task<CreateUserResponse> CreateUserAsync(User user, bool requireEmailVerification, params Role[] userRoles)
         {
             // create an ADB2C account - this can be removed once WozU has switched to Auth0 for SSO (client id and secret)
-            var adb2cUser = await _graphClient.CreateUser(user.Email, user.Email, user.Password);
-            Guid subscriberGuid = Guid.Parse(adb2cUser.AdditionalData["objectId"].ToString());
-            
+            Guid subscriberGuid = Guid.NewGuid();
+            try
+            {
+                var adb2cUser = await _graphClient.CreateUser(user.Email, user.Email, user.Password);
+                subscriberGuid = Guid.Parse(adb2cUser.AdditionalData["objectId"].ToString());
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation($"An exception occurred while trying to create an ADB2C user: {e.Message}");
+            }
+
             Auth0.ManagementApi.Models.User userCreationResponse = null;
             var subscriber = await _repositoryWrapper.SubscriberRepository.GetSubscriberByEmailAsync(user.Email);
 
