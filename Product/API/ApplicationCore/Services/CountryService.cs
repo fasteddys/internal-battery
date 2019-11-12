abf0b25ab49691105091dd91b007e1c9266955c4
Guin, Jyoti <jguin@allegisgroup.com>
2019-11-12 16:35:54 -0500

    beefing up error checking

--- Product/API/ApplicationCore/Services/CountryService.cs
+++ Product/API/ApplicationCore/Services/CountryService.cs
@@ -8,6 +8,7 @@ using UpDiddyApi.ApplicationCore.Interfaces;
 using AutoMapper;
 using UpDiddyLib.Domain.Models;
 using UpDiddyApi.Models;
+using UpDiddyApi.ApplicationCore.Exceptions;
 namespace UpDiddyApi.ApplicationCore.Services
 {
     public class CountryService : ICountryService
@@ -24,11 +25,15 @@ namespace UpDiddyApi.ApplicationCore.Services
 
         public async Task<CountryDetailDto> GetCountryDetail(Guid countryGuid)
         {
+            if (countryGuid == null || countryGuid == Guid.Empty)
+                throw new NullReferenceException("countryGuid cannot be null");
             string cacheKey = $"GetCountryDetail";
             IList<CountryDetailDto> rval = (IList<CountryDetailDto>)_memoryCacheService.GetCacheValue(cacheKey);
             if (rval == null)
             {
                 var countries = await _repositoryWrapper.Country.GetAllCountries();
+                if (countries == null)
+                    throw new NotFoundException("country not found");
                 rval = _mapper.Map<List<CountryDetailDto>>(countries);
                 _memoryCacheService.SetCacheValue(cacheKey, rval);
             }
@@ -38,8 +43,8 @@ namespace UpDiddyApi.ApplicationCore.Services
         public async Task<List<CountryDetailDto>> GetAllCountries(int limit = 100, int offset = 0, string sort = "modifyDate", string order = "descending")
         {
             var country = await _repositoryWrapper.Country.GetByConditionWithSorting(x => x.IsDeleted == 0, limit, offset, sort, order);
-            if(country == null)
-                throw  new NotSupportedException("Invalid parameters");
+            if (country == null)
+                throw new NotFoundException("Country not found");
             return _mapper.Map<List<CountryDetailDto>>(country);
         }
 
@@ -56,9 +61,11 @@ namespace UpDiddyApi.ApplicationCore.Services
 
         public async Task UpdateCountry(Guid countryGuid, CountryDetailDto countryDetailDto)
         {
-            if (countryDetailDto == null)
-                throw new NullReferenceException("countryDetailDto cannot be null");
+            if (countryDetailDto == null || countryGuid == null || countryGuid == Guid.Empty)
+                throw new NullReferenceException("countryDetailDto and countryGuid cannot be null");
             var country = await _repositoryWrapper.Country.GetbyCountryGuid(countryGuid);
+            if (country == null)
+                throw new NotFoundException("Country not found");
             country.OfficialName = countryDetailDto.OfficialName;
             country.DisplayName = countryDetailDto.DisplayName;
             country.Sequence = countryDetailDto.Sequence;
@@ -74,6 +81,8 @@ namespace UpDiddyApi.ApplicationCore.Services
             if (countryGuid == null || countryGuid == Guid.Empty)
                 throw new NullReferenceException("CountryGuid cannot be null");
             var country = await _repositoryWrapper.Country.GetbyCountryGuid(countryGuid);
+            if (country == null)
+                throw new NotFoundException("Country not found");
             country.IsDeleted = 1;
             _repositoryWrapper.Country.Update(country);
             await _repositoryWrapper.SaveAsync();
--- Product/API/ApplicationCore/Services/StateService.cs
+++ Product/API/ApplicationCore/Services/StateService.cs
@@ -8,6 +8,7 @@ using UpDiddyApi.ApplicationCore.Interfaces;
 using AutoMapper;
 using UpDiddyLib.Domain.Models;
 using UpDiddyApi.Models;
+using UpDiddyApi.ApplicationCore.Exceptions;
 namespace UpDiddyApi.ApplicationCore.Services
 {
     public class StateService : IStateService
@@ -24,12 +25,15 @@ namespace UpDiddyApi.ApplicationCore.Services
 
         public async Task<StateDetailDto> GetStateDetail(Guid stateGuid)
         {
+            if (stateGuid == null || stateGuid == Guid.Empty)
+                throw new NullReferenceException("stateGuid cannot be null");
             string cacheKey = $"GetStateDetail";
             IList<StateDetailDto> rval = (IList<StateDetailDto>)_memoryCacheService.GetCacheValue(cacheKey);
             if (rval == null)
             {
                 var states = await _repositoryWrapper.State.GetAllStatesAsync();
-                rval = _mapper.Map<List<StateDetailDto>>(states);
+                if (states == null)
+                    throw new NotFoundException("States not found");
                 _memoryCacheService.SetCacheValue(cacheKey, rval);
             }
             return rval?.Where(x => x.StateGuid == stateGuid).FirstOrDefault();
@@ -37,18 +41,24 @@ namespace UpDiddyApi.ApplicationCore.Services
 
         public async Task<List<StateDetailDto>> GetAllStates(Guid countryGuid, int limit = 100, int offset = 0, string sort = "modifyDate", string order = "descending")
         {
+            if (countryGuid == null || countryGuid == Guid.Empty)
+                throw new NullReferenceException("CountryGuid cannot be null");
             var country = await _repositoryWrapper.Country.GetbyCountryGuid(countryGuid);
+            if (country == null)
+                throw new NotFoundException("Country not found");
             var states = await _repositoryWrapper.State.GetByConditionWithSorting(x => x.CountryId == country.CountryId && x.IsDeleted == 0, limit, offset, sort, order);
             if (states == null)
-                throw new NotSupportedException("Invalid parameters");
+                throw new NotFoundException("States not found");
             return _mapper.Map<List<StateDetailDto>>(states);
         }
 
         public async Task CreateState(Guid countryGuid, StateDetailDto stateDetailDto)
         {
-            if (stateDetailDto == null)
-                throw new NullReferenceException("stateDetailDto cannot be null");
+            if (countryGuid == null || countryGuid == Guid.Empty || stateDetailDto == null)
+                throw new NullReferenceException("StateDetailDto and CountryGuid cannot be null");
             var country = await _repositoryWrapper.Country.GetbyCountryGuid(countryGuid);
+            if (country == null)
+                throw new NotFoundException("Country not found");
             var state = _mapper.Map<State>(stateDetailDto);
             state.CreateDate = DateTime.UtcNow;
             state.StateGuid = Guid.NewGuid();
@@ -59,10 +69,16 @@ namespace UpDiddyApi.ApplicationCore.Services
 
         public async Task UpdateState(Guid countryGuid, Guid stateGuid, StateDetailDto stateDetailDto)
         {
+            if (stateGuid == null || stateGuid == Guid.Empty || countryGuid == null || countryGuid == Guid.Empty)
+                throw new NullReferenceException("StateGuid and CountryGuid cannot be null");
             if (stateDetailDto == null)
-                throw new NullReferenceException("stateDetailDto cannot be null");
+                throw new NullReferenceException("StateDetailDto cannot be null");
             var country = await _repositoryWrapper.Country.GetbyCountryGuid(countryGuid);
+            if (country == null)
+                throw new NotFoundException("Country not found");
             var state = await _repositoryWrapper.State.GetByStateGuid(stateGuid);
+            if (state == null)
+                throw new NotFoundException("State not found");
             state.Name = stateDetailDto.Name;
             state.Sequence = stateDetailDto.Sequence;
             state.Code = stateDetailDto.Code;
@@ -75,8 +91,10 @@ namespace UpDiddyApi.ApplicationCore.Services
         public async Task DeleteState(Guid stateGuid)
         {
             if (stateGuid == null || stateGuid == Guid.Empty)
-                throw new NullReferenceException("stateGuid cannot be null");
+                throw new NullReferenceException("StateGuid cannot be null");
             var state = await _repositoryWrapper.State.GetByStateGuid(stateGuid);
+            if (state == null)
+                throw new NotFoundException("State not found");
             state.IsDeleted = 1;
             _repositoryWrapper.State.Update(state);
             await _repositoryWrapper.SaveAsync();
