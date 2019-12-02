5d422fe1a1e2d3e1b214fa4b71043c9480d9031e
Guin, Jyoti <jguin@allegisgroup.com>
2019-12-02 14:19:41 -0500

    checking in removal of unnessary properties

--- Product/UpDiddyLib/Domain/Models/TraitifyRequestDto.cs
+++ Product/UpDiddyLib/Domain/Models/TraitifyRequestDto.cs
@@ -1,28 +1,12 @@
-
-
 using System.ComponentModel.DataAnnotations;
 namespace UpDiddyLib.Domain.Models
 {
     public class TraitifyRequestDto
     {
-        // public Guid TraitifyGuid { get; set; }
-        // public Guid? SubscriberGuid { get; set; }
-        // public int? SubscriberId { get; set; }
-        // public string AssessmentId { get; set; }
-        public string AssessmentId {get;set;}
+        public string AssessmentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", ErrorMessage = "Invalid email address. Please update your supplied email and try again.")]
         public string Email { get; set; }
-        // public string Password { get; set; }
-        // public DateTime? CompleteDate { get; set; }
-        // public string DeckId { get; set; }
-        // public string ResultData { get; set; }
-        // public int ResultLength { get; set; }
-        // public string PublicKey { get; set; }
-        // public string Host { get; set; }
-        // public bool IsComplete { get; set; }
-        // public bool IsRegistered { get; set; }
-
     }
 }
