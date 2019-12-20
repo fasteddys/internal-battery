9fd6efb44ac409114377d83b110d39658b778f22
Guin, Jyoti <jguin@allegisgroup.com>
2019-12-20 12:14:49 -0500

    Fix for course favorite

--- Product/API/Migrations/20191219195720_Course_DesktopImage.cs
+++ Product/API/Migrations/20191219195720_Course_DesktopImage.cs
@@ -639,6 +639,7 @@ BEGIN
         ,v.VendorGuid
         ,cl.CourseLevelGuid as CourseLevelGuid		
 		,cl.Name as Level
+		,c.Code 
     FROM Course c
     JOIN CourseFavorite cf ON c.CourseId = cf.CourseId
     JOIN Subscriber s on cf.SubscriberId = s.SubscriberId
