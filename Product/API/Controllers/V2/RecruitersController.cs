805d052018b845cc7c831933925d745341a9c8d0
Brazil, Jim <jibrazil@populusgroup.com>
2020-01-22 10:31:41 -0500

    switching tasks

---
+++ Product/API/Controllers/V2/RecruitersController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UpDiddyApi.ApplicationCore.Services;
+using UpDiddyApi.Models;
+using UpDiddyLib.Dto;
+using AutoMapper;
+using System.Security.Claims;
+using UpDiddyApi.ApplicationCore.Interfaces.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using UpDiddyApi.ApplicationCore.Interfaces.Business;
+using Microsoft.AspNetCore.Http;
+using UpDiddyApi.ApplicationCore.Interfaces;
+using UpDiddyLib.Domain.Models;
+using UpDiddyApi.ApplicationCore.Exceptions;
+using UpDiddyLib.Shared.GoogleJobs;
+using System.Collections.Generic;
+
+namespace UpDiddyApi.Controllers.V2
+{
+    [Route("/V2/[controller]/")]
+    [ApiController]
+    public class RecruitersController : ControllerBase
+    {
+        private readonly UpDiddyDbContext _db = null;
+        private readonly IMapper _mapper;
+        private readonly Microsoft.Extensions.Configuration.IConfiguration _configuration;
+        private readonly ILogger _syslog;
+        private readonly IHttpClientFactory _httpClientFactory = null;
+        private readonly int _postingTTL = 30;        
+        private readonly IRepositoryWrapper _repositoryWrapper;
+        private readonly IServiceProvider _services;
+        private readonly IJobService _jobService;
+        private readonly IRecruiterService _recruiterService;
+
+
+        #region constructor 
+        public RecruitersController(IServiceProvider services
+        , IHangfireService hangfireService
+        , IRecruiterService recruiterService
+        , IJobFavoriteService jobFavoriteService
+        , IJobSearchService jobSearchService
+        , ICloudTalentService cloudTalentService
+        , ITrackingService trackingService
+        , IKeywordService keywordService)
+
+        {
+            _services = services;
+
+            _db = _services.GetService<UpDiddyDbContext>();
+            _mapper = _services.GetService<IMapper>();
+            _configuration = _services.GetService<Microsoft.Extensions.Configuration.IConfiguration>();
+            _syslog = _services.GetService<ILogger<JobController>>();
+            _httpClientFactory = _services.GetService<IHttpClientFactory>();
+            _repositoryWrapper = _services.GetService<IRepositoryWrapper>();            
+            _recruiterService = recruiterService;            
+        }
+
+        #endregion
+
+
+
+        [HttpGet]
+        [Authorize(Policy = "IsCareerCircleAdmin")]
+        public async Task<IActionResult> GetJobAdminForSubscriber(int limit = 10, int offset = 0, string sort = "modifyDate", string order = "descending")
+        {
+
+            //   JobCrudListDto postings = await _jobPostingService.GetJobPostingCrudForSubscriber(GetSubscriberGuid(), limit, offset, sort, order);
+            // return Ok(postings);
+
+            return Ok();
+        }
+
+
+
+    }
+}
---
+++ Product/UpDiddyLib/Domain/Models/RecruiterInfoDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpDiddyLib.Domain.Models
+{
+
+    public class RecruiterInfoListDto
+    {
+        public List<RecruiterInfoDto> Entities { get; set; }
+        public int TotalRecords { get; set; }
+    }
+
+    public class RecruiterInfoDto
+    {
+        public Guid RecruiterGuid { get; set; }
+        public Guid? SubscriberGuid { get; set; }
+        public Guid? CompanyGuid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }        
+        public bool IsInAuth0RecruiterGroupRecruiter { get; set; }
+    }
+}
