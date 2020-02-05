6efe092e6a903b6ce75e3b7e3b179874873f20e5
Bill Koenig <wikoenig@populusgroup.com>
2020-02-05 17:24:37 -0500

    integrating with tests in dev-careercircle

--- Product/API.Tests/AzureAPITests.cs
+++ Product/API.Tests/AzureAPITests.cs
@@ -1,268 +1,109 @@
-﻿using Auth0.AuthenticationApi;
-using Auth0.AuthenticationApi.Models;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 using Xunit;
-using Newtonsoft.Json;
+using Xunit.Abstractions;
 using API.Tests.Helpers;
 using System.Text;
 using System.Net.Http;
 using System.Security.Authentication;
-using Newtonsoft.Json.Schema;
+using System.Collections.Generic;
+using System;
 
 namespace API.Tests.AzureApi
 {
     public class DataDrivenApiEndpointTests
     {
+        private readonly ITestOutputHelper _output;
+
+        public DataDrivenApiEndpointTests(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
         [Theory]
-        [MemberData(nameof(AzureApiDataProvider.ParseOpenApiSpecificationForApiEndpoints), MemberType = typeof(AzureApiDataProvider))]
-        public void Validate_All_Api_Endpoints_Conform_To_Specifications(string apiEndpointTestName, MemberDataSerializer<ApiEndpointTest> apiEndpointTest)
+        [MemberData(nameof(AzureApiDataProvider.ExtractApiOperationsFromAllApis), MemberType = typeof(AzureApiDataProvider))]
+        public void Validate_All_Api_Endpoints_Conform_To_Specifications(MemberDataSerializer<ApiOperationTest> apiOperationTest)
         {
-            /* todo: 
-             * call each endpoint 
-             * assert actual response schema (not the data) against the intended response schema
-             * assert intended response status code to the actual response status code
-             * ensure that unit test responses contain the necessary information to take action if necessary
-             */
 
-            Assert.True(true);
-        }
-    }
+            // set up variables that will be used for the assertion
+            bool isActualStatusCodeMatchesExpectedStatusCode = false;
+            bool isResponseBodyMatchesResponseSchema = false;
 
-    public class ApiEndpointTest
-    {
-        public Uri Uri { get; set; }
-        public string HttpMethod { get; set; }
-        public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
-        public string RequestBody { get; set; }
-        public string RequestSchema { get; set; }
-        public string ResponseBody { get; set; }
-        public string ResponseSchema { get; set; }
-        public int StatusCode { get; set; }
-        public List<string> DefinitionErrors { get; set; } = new List<string>();
-    }
+            // emit some basic information about the test
+            _output.WriteLine("********************** BEGIN TEST **********************");
+            _output.WriteLine($"Operation Name: {apiOperationTest.Object.Name}");
+            _output.WriteLine($"Api Version: {apiOperationTest.Object.ApiVersion}");
+            _output.WriteLine($"Http Verb : {apiOperationTest.Object.HttpMethod.Method}");
+            _output.WriteLine($"Expected Status Code: {apiOperationTest.Object.ExpectedStatusCode}");
 
-    /// <summary>
-    /// Retrieves all of the Azure API endpoints using Azure Resource Management 
-    /// </summary>
-    public class AzureApiDataProvider
-    {
-        public static Dictionary<string, string> GetUrlParameterReplacementValues()
-        {
-            var urlParameterReplacementValues = new Dictionary<string, string>();
-            var configuration = ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
-
-            var configurationSectionWithReplacementValues = configuration.GetSection("UrlParameterReplacementValues").GetChildren();
-            foreach (var urlParameterReplacementValue in configurationSectionWithReplacementValues)
+            if (apiOperationTest.Object.DefinitionErrors.Count == 0)
             {
-                urlParameterReplacementValues.Add(urlParameterReplacementValue.Key, urlParameterReplacementValue.Value);
-            }
-
-            return urlParameterReplacementValues;
-        }
-
-        /// <summary>
-        /// Constructs a Uri which can be used to retrieve the OpenApi specification for an Azure API
-        /// </summary>
-        /// <returns></returns>
-        public static Uri GetOpenApiRequestUri()
-        {
-            var configuration = ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
-
-            StringBuilder url = new StringBuilder("https://");
-            url.Append(configuration["AzureResourceManagement:ResourceName"]);
-            url.Append(".management.azure-api.net/subscriptions/");
-            url.Append(configuration["AzureResourceManagement:SubscriptionId"]);
-            url.Append("/resourceGroups/");
-            url.Append(configuration["AzureResourceManagement:ResourceGroupId"]);
-            url.Append("/providers/Microsoft.ApiManagement/service/");
-            url.Append(configuration["AzureResourceManagement:ServiceId"]);
-            url.Append("/apis/");
-            url.Append(configuration["AzureResourceManagement:ApiId"]);
-            url.Append("?format=openapi-link-json&export=true&api-version=2019-01-01");
-            return new Uri(url.ToString(), UriKind.Absolute);
-        }
-
-        /// <summary>
-        /// Using a test subscriber for the current environment, authenticates with Auth0 and retrieves a valid JWT
-        /// </summary>
-        /// <returns></returns>
-        public static async Task<string> GetValidJwtFromTestSubscriber()
-        {
-            var configuration = ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
-
-            AuthenticationApiClient _auth0Client = new AuthenticationApiClient(new Uri(configuration["Auth0:Url"].ToString()));
-            var result = await _auth0Client.GetTokenAsync(new ResourceOwnerTokenRequest
-            {
-                ClientId = configuration["Auth0:ClientId"],
-                ClientSecret = configuration["Auth0:ClientSecret"],
-                Scope = "openid profile email",
-                Realm = "Username-Password-Authentication",
-                Username = configuration["TestSubscriber:Username"],
-                Password = configuration["TestSubscriber:Password"],
-                Audience = configuration["Auth0:Audience"]
-            });
-
-            return result.AccessToken;
-        }
-        /// <summary>
-        /// Submits a web request for an Azure Api using Azure Resource Management and returns the response as JSON
-        /// </summary>
-        /// <param name="openApiRequestUri"></param>
-        /// <returns></returns>
-        public static JObject LoadOpenApiSpecification(Uri openApiRequestUri)
-        {
-            HttpClient client = new HttpClient(new HttpClientHandler() { SslProtocols = SslProtocols.Tls12 });
-            var response = client.GetAsync(GetOpenApiRequestUri()).Result;
-            if (response.StatusCode != HttpStatusCode.OK)
-                throw new ApplicationException($"Invalid status code while retrieving Api Endpoints: {response.StatusCode.ToString()}");
-
-            return response.Content.ReadAsAsync<JObject>().Result;
-        }
-
-        /// <summary>
-        /// Retrieves a valid subscription key value for use with Azure Api
-        /// </summary>
-        /// <returns></returns>
-        public static string GetSubscriptionKeyValue()
-        {
-            var configuration = ConfigurationHelper.GetConfiguration(Directory.GetCurrentDirectory());
-            return configuration["SubscriptionKeyValue"].ToString();
-        }
-
-        /// <summary>
-        /// Parses the OpenApi specification and constructs tests for all Api endpoints  
-        /// </summary>
-        /// <returns></returns>
-        public static IEnumerable<object[]> ParseOpenApiSpecificationForApiEndpoints()
-        {
-            // retrieve a valid Jwt for the test subscriber
-            var subscriberJwt = GetValidJwtFromTestSubscriber().Result;
-
-            // load the OpenApi specification from Azure Resource Management
-            JObject openApiSpecification = LoadOpenApiSpecification(GetOpenApiRequestUri());
-
-            // get all url replacement values from app settings for testing entity-specific endpoints
-            Dictionary<string, string> urlParameterReplacementValues = GetUrlParameterReplacementValues();
-
-            // construct the base url that will be used for all api endpoint tests (do not need to support header or query string right now)
-            string serviceUrl = openApiSpecification.Root.SelectToken("$.serviceUrl").Value<string>();
-            string path = openApiSpecification.Root.SelectToken("$.path").Value<string>();
-            string apiVersion = openApiSpecification.Root.SelectToken("$.apiVersion").Value<string>();
-            string baseUrl = Utilities.UrlCombine(Utilities.UrlCombine(serviceUrl, path), apiVersion);
-
-            // get valid subscription for use with the Azure Api - this needs to be added to every request
-            var subscriptionKeyParameterKey = openApiSpecification.Root.SelectToken("$.subscriptionKeyParameterNames.header").Value<string>();
-            string subscriptionKeyParameterValue = GetSubscriptionKeyValue();
-
-            // get list of api endpoints
-            var operations = openApiSpecification.Root.SelectTokens("$.operations.value");
-
-            // schemas contain definitions for all operations (use these to compare responses from service requests)
-            var schemas = openApiSpecification.Root.SelectToken("$.schemas.value[0].document.definitions");
-
-            // call each endpoint   
-            foreach (var operation in operations.Children<JToken>())
-            {
-                var httpMethod = operation.SelectToken("$.method").Value<string>();
-                var urlTemplate = operation.SelectToken("$.urlTemplate").Value<string>();
-                var operationName = operation.SelectToken("$.name").Value<string>();
-
-                // if the url contains parameters, set it using a test value from the config file
-                var replacement = urlParameterReplacementValues
-                    .Where(rv => urlTemplate.Contains(rv.Key))
-                    .Select(rv => new { Key = rv.Key, Value = rv.Value })
-                    .FirstOrDefault();
-
-                if (replacement != null)
-                    urlTemplate = urlTemplate.Replace(replacement.Key, replacement.Value);
-
-
-
-
-                // check to see if a 401 response is possible. if it is, we need to add a valid subscriber JWT to the request when testing successful responses
-                // todo: one without a valid jwt (to ensure that the endpoint is secured)
-                // todo: one with a valid jwt (to ensure that the endpoint works)
-                bool isRequiresSubscriberJwtForSuccessfulResponse = (operation.SelectToken("$.responses[?(@.statusCode == 401)]") != null);
-
-                foreach (var response in operation.SelectTokens("$.responses[0]"))
+                // only execute the test if there were no definition errors
+                HttpClient client = new HttpClient(new HttpClientHandler() { SslProtocols = SslProtocols.Tls12 });
+                var request = new HttpRequestMessage
                 {
-                    ApiEndpointTest apiEndpointTest = new ApiEndpointTest();
-                    apiEndpointTest.HttpMethod = httpMethod;
-                    apiEndpointTest.Uri = new Uri(Utilities.UrlCombine(baseUrl, urlTemplate));
+                    RequestUri = apiOperationTest.Object.Uri,
+                    Method = apiOperationTest.Object.HttpMethod,
+                };
+                foreach (var header in apiOperationTest.Object.Headers)
+                {
+                    request.Headers.Add(header.Key, header.Value);
+                }
+                if (!string.IsNullOrWhiteSpace(apiOperationTest.Object.RequestBody))
+                {
+                    request.Content = new StringContent(apiOperationTest.Object.RequestBody, Encoding.UTF8, "application/json");
+                }
 
-                    // todo: apiEndpointTest.RequestBody - how will we determine what values to put here - use definition/schema?
-                    var rawRequestBody = operation.SelectToken("$.request.representations[0].sample");                    
-                    if (rawRequestBody != null)
-                    {
-                        try
-                        {
-                            // todo: parse as json, does that succeed, compare to schema for associated type, is that valid
-                            apiEndpointTest.RequestBody = rawRequestBody.Value<string>();                            
-                        }
-                        catch(Exception e)
-                        {
-                            apiEndpointTest.DefinitionErrors.Add($"A sample request body was found but could not be parsed; error message: {e.Message}");
-                        }
-                    }
-                        // assume that every request is protected with a subscription
-                        apiEndpointTest.Headers.Add(subscriptionKeyParameterKey, subscriptionKeyParameterValue);
-                    if (isRequiresSubscriberJwtForSuccessfulResponse)
-                        apiEndpointTest.Headers.Add("Authorization", $"Bearer {subscriberJwt}");
+                var response = client.SendAsync(request).Result;
+                isActualStatusCodeMatchesExpectedStatusCode = (int)response.StatusCode == apiOperationTest.Object.ExpectedStatusCode;
 
-                    // use the status code from the response definition as the expected status code for the api endpoint test
-                    var statusCode = response.SelectToken("$.statusCode").Value<int>();
-                    switch (statusCode)
-                    {
-                        case 200:
-                        case 201:
-                        case 202:
-                        case 204:
-                            apiEndpointTest.StatusCode = statusCode;
-                            break;
-                        case 401:
-                            // remove JWT if it exists. this should cause a 401 response.
-                            apiEndpointTest.StatusCode = statusCode;
-                            apiEndpointTest.Headers.Remove("Authorization");
-                            break;
-                        case 400:
-                            // TODO: need to think about how to implement tests to specifically test how endpoints handle malformed requests
-                            break;
-                        default:
-                            apiEndpointTest.DefinitionErrors.Add($"Unrecognized response status code: {statusCode.ToString()}");
-                            break;
-                    }
-
-                    // use the type name from the representation to retrieve the schema, load the example, ensure that the example is valid, then save the schema with the test
+                if (apiOperationTest.Object.ResponseSchema != null)
+                {
                     try
                     {
-                        var representation = response.SelectToken("$.representations[0]");
-                        if (representation != null)
-                        {
-                            string typeName = representation.SelectToken("$.typeName").Value<string>();
-                            string escapedTypeName = $"['{typeName}']";
-                            var responseSchema = schemas.SelectToken($"$.{escapedTypeName}");
-                            JSchema jschema = JSchema.Parse(responseSchema.ToString(Formatting.None));
-                            JObject example = responseSchema["example"].Value<JObject>();
-                            if (!example.IsValid(jschema))
-                                throw new ApplicationException("Response example is not valid according to the schema definition.");
-                            apiEndpointTest.ResponseSchema = jschema.ToString(SchemaVersion.Unset);
-                        }
+                        var bodyContent = response.Content.ReadAsAsync<JObject>().Result;
+                        isResponseBodyMatchesResponseSchema = bodyContent.IsValid(apiOperationTest.Object.ResponseSchema);
                     }
                     catch (Exception e)
                     {
-                        apiEndpointTest.DefinitionErrors.Add($"Schema not found in response representation; error message: {e.Message}");
+                        apiOperationTest.Object.DefinitionErrors.Add($"An error occurred while trying to validate the response: {e.Message}");
                     }
-
-                    // add the api operation to the set of data-driven tests
-                    yield return new object[] { $"{operationName} ({statusCode})", new MemberDataSerializer<ApiEndpointTest>(apiEndpointTest) };
                 }
+                else
+                    isResponseBodyMatchesResponseSchema = true;
 
+                // todo: perform cleanup for create, update, and delete operations (using TargetedObjectGuid)
+                //if (apiOperationTest.Object..HasValue)
+                //{
+                //}
+                // need separate logic for create - how to isolate the object created - parse response...
+
+            }
+            else
+            {
+                // output any definition errors that exist
+                _output.WriteLine("Api operation was not invoked due to definition errors; see below for details:");
+                foreach (var definitionError in apiOperationTest.Object.DefinitionErrors)
+                {
+                    _output.WriteLine($"Definition error: {definitionError}");
+                }
+            }
+
+            try
+            {
+                Assert.True(
+                    isActualStatusCodeMatchesExpectedStatusCode
+                    && isResponseBodyMatchesResponseSchema
+                    && apiOperationTest.Object.DefinitionErrors.Count == 0,
+                    $"isActualStatusCodeMatchesExpectedStatusCode: {isActualStatusCodeMatchesExpectedStatusCode.ToString()}, isResponseBodyMatchesResponseSchema: {isResponseBodyMatchesResponseSchema.ToString()}");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                _output.WriteLine("*********************** END TEST ***********************");
             }
         }
     }
---
+++ Product/API.Tests/Helpers/ApiOperationTest.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace API.Tests.Helpers
+{
+    /// <summary>
+    /// Contains all information for an operation within an api which is necessary in order to execute an integration test
+    /// </summary>
+    public class ApiOperationTest
+    {
+        #region Constructor 
+
+        public ApiOperationTest(bool isRequiresSubscriberJwtForSuccessfulResponse,
+            string httpMethod,
+            string urlTemplate,
+            string operationName,
+            JToken schemas,
+            Dictionary<string, string> urlParameterReplacementValues,
+            string baseUrl,
+            string subscriptionKeyParameterKey,
+            string subscriptionKeyParameterValue,
+            string subscriberJwt,
+            JToken request,
+            JToken response,
+            KeyValuePair<string, string> apiVersion)
+        {
+            // if the url contains parameters, set them using test values from the config file
+            var replacements = urlParameterReplacementValues
+                .Where(rv => urlTemplate.Contains(rv.Key))
+                .Select(rv => new { Key = rv.Key, Value = rv.Value });
+            if (replacements != null)
+            {
+                foreach(var replacement in replacements)
+                {
+                    urlTemplate = urlTemplate.Replace(replacement.Key, replacement.Value);
+                    Guid targetedObjectGuid;
+                    if (!Guid.TryParse(replacement.Value, out targetedObjectGuid))
+                        this.DefinitionErrors.Add($"Unable to cast replacement value to guid; key: {replacement.Key}, value: {replacement.Value}");
+                    else
+                        this.TargetedObjectIds.Add(targetedObjectGuid);
+                }                
+            }
+
+            this.HttpMethod = new HttpMethod(httpMethod);
+            this.Uri = new Uri(Utilities.UrlCombine(baseUrl, urlTemplate));
+            this.ApiVersion = apiVersion;
+            this.Headers.Add(apiVersion.Key, apiVersion.Value);
+
+            // expect a payload in the request body if the operation is a POST or a PUT
+            if (this.HttpMethod.Method == "POST" || this.HttpMethod.Method == "PUT")
+            {
+                try
+                {
+                    var rawRequestBody = request.SelectToken("$.representations[0].sample");
+                    if (rawRequestBody == null || string.IsNullOrWhiteSpace(rawRequestBody.Value<string>()))
+                        this.DefinitionErrors.Add("No sample request payload was found for this operation.");
+                    else
+                    {
+                        // ensure that the sample payload is valid JSON
+                        var jsonRequestBody = JObject.Parse(rawRequestBody.Value<string>());
+
+                        // add the sample to the request body
+                        this.RequestBody = jsonRequestBody.ToString(Formatting.None);
+                    }
+                }
+                catch (JsonReaderException jre)
+                {
+                    this.DefinitionErrors.Add($"The sample request payload was not valid JSON: {jre.Message}");
+                }
+                catch (Exception e)
+                {
+                    this.DefinitionErrors.Add($"The sample request payload was not loaded: {e.Message}");
+                }
+            }
+
+            // assume that every request is protected with a subscription
+            this.Headers.Add(subscriptionKeyParameterKey, subscriptionKeyParameterValue);
+            if (isRequiresSubscriberJwtForSuccessfulResponse)
+                this.Headers.Add("Authorization", $"Bearer {subscriberJwt}");
+
+            // use the status code from the response definition as the expected status code for the api endpoint test
+            var statusCode = response.SelectToken("$.statusCode").Value<int>();
+            switch (statusCode)
+            {
+                case 200:
+                case 201:
+                case 202:
+                case 204:
+                    this.ExpectedStatusCode = statusCode;
+                    break;
+                case 401:
+                    // remove JWT if it exists as this should cause a 401 response. remove definition error that requires sample payload (if one exists)
+                    this.ExpectedStatusCode = statusCode;
+                    this.Headers.Remove("Authorization");
+                    this.DefinitionErrors.Remove("No sample request payload was found for this operation.");
+                    break;
+                case 400:
+                    // TODO: need to think about how to implement tests to specifically test how endpoints handle malformed requests.
+                    // for now, remove all occurrences of the 400 status code from operation responses
+                    this.DefinitionErrors.Add("400 response status codes are not supported for automated tests; see code comments for details");
+                    break;
+                default:
+                    this.DefinitionErrors.Add($"Unexpected response status code: {statusCode.ToString()}");
+                    break;
+            }
+
+            // use the type name from the representation to retrieve the schema, load the example, ensure that the example is valid, then save the schema with the test
+            try
+            {
+                var representation = response.SelectToken("$.representations[0]");
+                if (representation != null)
+                {
+                    if (representation.SelectToken($".typeName") != null)
+                    {
+                        string typeName = representation.SelectToken("$.typeName").Value<string>();
+                        string escapedTypeName = $"['{typeName}']";
+                        var responseSchema = schemas.SelectToken($"$.{escapedTypeName}");
+                        if (responseSchema != null)
+                        {
+                            JSchema jschema = JSchema.Parse(responseSchema.ToString(Formatting.None));
+                            JObject example = responseSchema["example"].Value<JObject>();
+                            if (!example.IsValid(jschema))
+                                this.DefinitionErrors.Add("The response example is not valid according to the schema definition.");
+                            else
+                                this.ResponseSchema = jschema;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                this.DefinitionErrors.Add($"An error occurred while attempting to set the response schema; message: {e.Message}, stack trace: {e.StackTrace}");
+            }
+
+            this.Name = $"{operationName} ({statusCode})";
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; set; }
+        public Uri Uri { get; set; }
+        public HttpMethod HttpMethod { get; set; }
+        public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
+        public string RequestBody { get; set; }
+        public string ResponseBody { get; set; }
+        public JSchema ResponseSchema { get; set; }
+        public int ExpectedStatusCode { get; set; }
+        public KeyValuePair<string, string> ApiVersion { get; set; }
+        public List<string> DefinitionErrors { get; set; } = new List<string>();
+        public List<Guid> TargetedObjectIds { get; private set; } = new List<Guid>();
+
+        #endregion
+    }
+}
