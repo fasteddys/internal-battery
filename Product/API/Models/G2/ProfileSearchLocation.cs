70020e3d2e8e886fd1d743f45c49cd0720551c87
Bill Koenig <wikoenig@populusgroup.com>
2020-02-25 13:43:14 -0500

    additional data model changes and fixes for v_ProfileAzureSearch view

--- Product/API/Models/G2/ProfileSearchLocation.cs
+++ Product/API/Models/G2/ProfileSearchLocation.cs
@@ -14,8 +14,10 @@ namespace UpDiddyApi.Models.G2
         public Guid ProfileSearchLocationGuid { get; set; }
         public int ProfileId { get; set; }
         public virtual Profile Profile { get; set; }
-        public int CityId { get; set; }
+        public int? CityId { get; set; }
         public virtual City City { get; set; }
+        public int? StateId { get; set; }
+        public virtual State State { get; set; }
         public int? PostalId { get; set; }
         public virtual Postal Postal { get; set; }
         [DefaultValue(25)]
