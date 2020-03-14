f12cbf9a8b602bf239e14ad0d2f575ac4c75941e
Your Name <brferree@allegisgroup.com>
2020-03-14 10:14:52 -0400

    added exception handling to isolate production job application error - job app saved but no email is being received as a result

--- Product/API/ApplicationCore/Services/JobApplicationService.cs
+++ Product/API/ApplicationCore/Services/JobApplicationService.cs
@@ -89,69 +89,82 @@ namespace UpDiddyApi.ApplicationCore.Services
                 _db.JobApplication.Add(jobApplication);
                 _db.SaveChanges();
 
-                Stream SubscriberResumeAsStream = await _subscriberService.GetResumeAsync(subscriber);
-                SubscriberResumeAsStream.Seek(0, SeekOrigin.Begin);
-                string resumeEncoded = Convert.ToBase64String(Utils.StreamToByteArray(SubscriberResumeAsStream));
-
-                bool IsExternalRecruiter = jobPosting.Recruiter.Subscriber == null;
-
-                string RecruiterEmailToUse = jobPosting.Recruiter.Subscriber?.Email ?? jobPosting.Recruiter.Email;
-                // Create a jobposting dto needed for the fully qualified job posting url in the recuriter email
-                UpDiddyLib.Dto.JobPostingDto jobPostingDto = _mapper.Map<UpDiddyLib.Dto.JobPostingDto>(jobPosting);
-                // Send recruiter email alerting them to application
-
-                Dictionary<string, bool> EmailAddressesToSend = new Dictionary<string, bool>();
-                EmailAddressesToSend.Add(RecruiterEmailToUse, IsExternalRecruiter);
-                var VipEmails = _configuration.GetSection("SysEmail:VIPEmails").GetChildren();
-
-                // Ensure all VIPs get the internal email template
-                foreach (IConfigurationSection Child in VipEmails)
+                int emailStep =0;
+                try
                 {
-                    string VipEmail = Child.Value;
-                    EmailAddressesToSend.Add(VipEmail, false);
-                }
+                        emailStep=1;
 
-                foreach (string Email in EmailAddressesToSend.Keys)
-                {
-                    _hangfireService.Enqueue(() => _sysEmail.SendTemplatedEmailAsync
-                    (
-                        Email,
-                        _configuration["SysEmail:Transactional:TemplateIds:JobApplication-Recruiter" +
-                            (EmailAddressesToSend[Email] == true ? "-External" : string.Empty)],
-                        new
+                        Stream SubscriberResumeAsStream = await _subscriberService.GetResumeAsync(subscriber);
+                        SubscriberResumeAsStream.Seek(0, SeekOrigin.Begin);
+                        string resumeEncoded = Convert.ToBase64String(Utils.StreamToByteArray(SubscriberResumeAsStream));
+                        
+                        emailStep=2;
+                        bool IsExternalRecruiter = jobPosting.Recruiter.Subscriber == null;
+
+                        string RecruiterEmailToUse = jobPosting.Recruiter.Subscriber?.Email ?? jobPosting.Recruiter.Email;
+                        // Create a jobposting dto needed for the fully qualified job posting url in the recuriter email
+                        UpDiddyLib.Dto.JobPostingDto jobPostingDto = _mapper.Map<UpDiddyLib.Dto.JobPostingDto>(jobPosting);
+                        // Send recruiter email alerting them to application
+
+                        emailStep=3;
+                        Dictionary<string, bool> EmailAddressesToSend = new Dictionary<string, bool>();
+                        EmailAddressesToSend.Add(RecruiterEmailToUse, IsExternalRecruiter);
+                        var VipEmails = _configuration.GetSection("SysEmail:VIPEmails").GetChildren();
+
+                        emailStep=4;
+                        // Ensure all VIPs get the internal email template
+                        foreach (IConfigurationSection Child in VipEmails)
                         {
-                            ApplicantName = applicationDto.FirstName + " " + applicationDto.LastName,
-                            ApplicantFirstName = applicationDto.FirstName,
-                            ApplicantLastName = applicationDto.LastName,
-                            ApplicantEmail = subscriber.Email,
-                            JobTitle = jobPosting.Title,
-                            ApplicantUrl = SubscriberFactory.JobseekerUrl(_configuration, subscriber.SubscriberGuid.Value),
-                            JobUrl = JobPostingFactory.JobPostingFullyQualifiedUrl(_configuration, jobPostingDto),
-                            Subject = (IsExternalRecruiter == true ? $"{jobPosting.Company.CompanyName} job posting via CareerCircle" : "Applicant Alert"),
-                            RecruiterGuid = jobPosting.Recruiter.RecruiterGuid,
-                            JobApplicationGuid = jobApplication.JobApplicationGuid
-                        },
-                        Constants.SendGridAccount.Transactional,
-                        null,
-                        new List<Attachment>
+                            string VipEmail = Child.Value;
+                            EmailAddressesToSend.Add(VipEmail, false);
+                        }
+
+                        emailStep=5;
+                        foreach (string Email in EmailAddressesToSend.Keys)
                         {
-                            new Attachment
-                            {
-                                Content = resumeEncoded,
-                                Filename = Path.GetFileName(subscriber.SubscriberFile.FirstOrDefault().BlobName),
-                                Type=subscriber.SubscriberFile.FirstOrDefault().MimeType
-                            },
-                            new Attachment
-                            {
-                                Content = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(applicationDto.CoverLetter)),
-                                Filename = "CoverLetter.txt"
-                            }
-                        },
-                        null,
-                        null
-                    ));
+                            _hangfireService.Enqueue(() => _sysEmail.SendTemplatedEmailAsync
+                            (
+                                Email,
+                                _configuration["SysEmail:Transactional:TemplateIds:JobApplication-Recruiter" +
+                                    (EmailAddressesToSend[Email] == true ? "-External" : string.Empty)],
+                                new
+                                {
+                                    ApplicantName = applicationDto.FirstName + " " + applicationDto.LastName,
+                                    ApplicantFirstName = applicationDto.FirstName,
+                                    ApplicantLastName = applicationDto.LastName,
+                                    ApplicantEmail = subscriber.Email,
+                                    JobTitle = jobPosting.Title,
+                                    ApplicantUrl = SubscriberFactory.JobseekerUrl(_configuration, subscriber.SubscriberGuid.Value),
+                                    JobUrl = JobPostingFactory.JobPostingFullyQualifiedUrl(_configuration, jobPostingDto),
+                                    Subject = (IsExternalRecruiter == true ? $"{jobPosting.Company.CompanyName} job posting via CareerCircle" : "Applicant Alert"),
+                                    RecruiterGuid = jobPosting.Recruiter.RecruiterGuid,
+                                    JobApplicationGuid = jobApplication.JobApplicationGuid
+                                },
+                                Constants.SendGridAccount.Transactional,
+                                null,
+                                new List<Attachment>
+                                {
+                                    new Attachment
+                                    {
+                                        Content = resumeEncoded,
+                                        Filename = Path.GetFileName(subscriber.SubscriberFile.FirstOrDefault().BlobName),
+                                        Type=subscriber.SubscriberFile.FirstOrDefault().MimeType
+                                    },
+                                    new Attachment
+                                    {
+                                        Content = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(applicationDto.CoverLetter)),
+                                        Filename = "CoverLetter.txt"
+                                    }
+                                },
+                                null,
+                                null
+                            ));
+                        }
                 }
-                
+                catch(Exception ex)
+                {
+                    _syslog.Log("EMAIL EXCEPTION: "+ex.ToString()+" Email Step was: "+emailStep);
+                }
                 _syslog.Log(LogLevel.Information, $"***** JobApplicationController:CreateJobApplication completed at: {DateTime.UtcNow.ToLongDateString()}");
                 return jobApplication.JobApplicationGuid;
             }
