eda25ac0b489764b9ae0f6361f6bbe2a7bbb66af
Your Name <brferree@allegisgroup.com>
2020-03-14 11:43:27 -0400

    changed default email address to webmaster

--- Product/API/ApplicationCore/Services/JobApplicationService.cs
+++ Product/API/ApplicationCore/Services/JobApplicationService.cs
@@ -92,112 +92,127 @@ namespace UpDiddyApi.ApplicationCore.Services
                 int emailStep =0;
                 try
                 {
-                        emailStep=1;
+                    emailStep=1;
 
-                        Stream SubscriberResumeAsStream = await _subscriberService.GetResumeAsync(subscriber);
-                        SubscriberResumeAsStream.Seek(0, SeekOrigin.Begin);
-                        string resumeEncoded = Convert.ToBase64String(Utils.StreamToByteArray(SubscriberResumeAsStream));
-                        
-                        emailStep=2;
-                        bool IsExternalRecruiter = false;
-                        if ( jobPosting.Recruiter != null && jobPosting.Recruiter.Subscriber != null )
+                    Stream SubscriberResumeAsStream = await _subscriberService.GetResumeAsync(subscriber);
+                    SubscriberResumeAsStream.Seek(0, SeekOrigin.Begin);
+                    string resumeEncoded = Convert.ToBase64String(Utils.StreamToByteArray(SubscriberResumeAsStream));
+                    
+                    emailStep=2;
+                    bool IsExternalRecruiter = false;
+                    if ( jobPosting.Recruiter != null && jobPosting.Recruiter.Subscriber != null )
+                    {
+                        IsExternalRecruiter= true;
+                    }
+
+
+                    string RecruiterEmailToUse = string.Empty;
+                    RecruiterEmailToUse = "webmaster@careercircle.com"; 
+
+                    emailStep=3;
+                    try
+                    {
+                    //RecruiterEmailToUse = jobPosting.Recruiter.Subscriber?.Email ?? jobPosting.Recruiter.Email;
+                        if (jobPosting !=null)
                         {
-                            IsExternalRecruiter= true;
-                        }
-
-
-                        string RecruiterEmailToUse = string.Empty;
-                        RecruiterEmailToUse = "webmaster@careercircle.com"; 
-
-                        emailStep=3;
-                        try
-                        {
-                        //RecruiterEmailToUse = jobPosting.Recruiter.Subscriber?.Email ?? jobPosting.Recruiter.Email;
-                            if (jobPosting !=null)
+                            if (jobPosting.Recruiter !=null )
                             {
-                                if (jobPosting.Recruiter !=null )
+                                if (jobPosting.Recruiter.Subscriber !=null)
                                 {
-                                    if (jobPosting.Recruiter.Subscriber !=null)
+                                    if (jobPosting.Recruiter.Subscriber.Email != null)
                                     {
-                                        if (jobPosting.Recruiter.Subscriber.Email != null)
-                                        {
-                                            RecruiterEmailToUse = jobPosting.Recruiter.Subscriber?.Email;
-                                        }
+                                        RecruiterEmailToUse = jobPosting.Recruiter.Subscriber?.Email;
                                     }
-                                    else
+                                }
+                                else
+                                {
+                                    if (jobPosting.Recruiter.Email!=null)
                                     {
-                                        if (jobPosting.Recruiter.Email!=null)
-                                        {
-                                            RecruiterEmailToUse = jobPosting.Recruiter.Email;
-                                        }
+                                        RecruiterEmailToUse = jobPosting.Recruiter.Email;
                                     }
                                 }
                             }
                         }
-                        catch(Exception iex)
-                        {
-                                RecruiterEmailToUse = "bferree@careercircle.com"; 
-                        }
+                    }
+                    catch(Exception iex)
+                    {
+                        RecruiterEmailToUse = "bferree@careercircle.com"; 
+                    }
 
-                        emailStep=4;
-                        // Create a jobposting dto needed for the fully qualified job posting url in the recuriter email
-                        UpDiddyLib.Dto.JobPostingDto jobPostingDto = _mapper.Map<UpDiddyLib.Dto.JobPostingDto>(jobPosting);
-                      
-                        // Send recruiter email alerting them to application
-                        emailStep=5;
-                        Dictionary<string, bool> EmailAddressesToSend = new Dictionary<string, bool>();
-                        EmailAddressesToSend.Add(RecruiterEmailToUse, IsExternalRecruiter);
-                        var VipEmails = _configuration.GetSection("SysEmail:VIPEmails").GetChildren();
+                    emailStep=4;
+                    // Create a jobposting dto needed for the fully qualified job posting url in the recuriter email
+                    UpDiddyLib.Dto.JobPostingDto jobPostingDto = _mapper.Map<UpDiddyLib.Dto.JobPostingDto>(jobPosting);
+                    
+                    // Send recruiter email alerting them to application
+                    emailStep=5;
+                    Dictionary<string, bool> EmailAddressesToSend = new Dictionary<string, bool>();
+                    EmailAddressesToSend.Add(RecruiterEmailToUse, IsExternalRecruiter);
+                    var VipEmails = _configuration.GetSection("SysEmail:VIPEmails").GetChildren();
 
-                        emailStep=6;
-                        // Ensure all VIPs get the internal email template
-                        foreach (IConfigurationSection Child in VipEmails)
-                        {
-                            string VipEmail = Child.Value;
-                            EmailAddressesToSend.Add(VipEmail, false);
-                        }
+                    emailStep=6;
+                    // Ensure all VIPs get the internal email template
+                    foreach (IConfigurationSection Child in VipEmails)
+                    {
+                        string VipEmail = Child.Value;
+                        EmailAddressesToSend.Add(VipEmail, false);
+                    }
 
-                        emailStep=7;
+                    emailStep=7;
+                    try
+                    {
                         foreach (string Email in EmailAddressesToSend.Keys)
                         {
-                            _hangfireService.Enqueue(() => _sysEmail.SendTemplatedEmailAsync
-                            (
-                                Email,
-                                _configuration["SysEmail:Transactional:TemplateIds:JobApplication-Recruiter" +
-                                    (EmailAddressesToSend[Email] == true ? "-External" : string.Empty)],
-                                new
-                                {
-                                    ApplicantName = applicationDto.FirstName + " " + applicationDto.LastName,
-                                    ApplicantFirstName = applicationDto.FirstName,
-                                    ApplicantLastName = applicationDto.LastName,
-                                    ApplicantEmail = subscriber.Email,
-                                    JobTitle = jobPosting.Title,
-                                    ApplicantUrl = SubscriberFactory.JobseekerUrl(_configuration, subscriber.SubscriberGuid.Value),
-                                    JobUrl = JobPostingFactory.JobPostingFullyQualifiedUrl(_configuration, jobPostingDto),
-                                    Subject = (IsExternalRecruiter == true ? $"{jobPosting.Company.CompanyName} job posting via CareerCircle" : "Applicant Alert"),
-                                    RecruiterGuid = jobPosting.Recruiter.RecruiterGuid,
-                                    JobApplicationGuid = jobApplication.JobApplicationGuid
-                                },
-                                Constants.SendGridAccount.Transactional,
-                                null,
-                                new List<Attachment>
-                                {
-                                    new Attachment
+                            
+                            try
+                            {
+                                _hangfireService.Enqueue(() => _sysEmail.SendTemplatedEmailAsync
+                                (
+                                    Email,
+                                    _configuration["SysEmail:Transactional:TemplateIds:JobApplication-Recruiter" +
+                                        (EmailAddressesToSend[Email] == true ? "-External" : string.Empty)],
+                                    new
                                     {
-                                        Content = resumeEncoded,
-                                        Filename = Path.GetFileName(subscriber.SubscriberFile.FirstOrDefault().BlobName),
-                                        Type=subscriber.SubscriberFile.FirstOrDefault().MimeType
+                                        ApplicantName = applicationDto.FirstName + " " + applicationDto.LastName,
+                                        ApplicantFirstName = applicationDto.FirstName,
+                                        ApplicantLastName = applicationDto.LastName,
+                                        ApplicantEmail = subscriber.Email,
+                                        JobTitle = jobPosting.Title,
+                                        ApplicantUrl = SubscriberFactory.JobseekerUrl(_configuration, subscriber.SubscriberGuid.Value),
+                                        JobUrl = JobPostingFactory.JobPostingFullyQualifiedUrl(_configuration, jobPostingDto),
+                                        Subject = (IsExternalRecruiter == true ? $"{jobPosting.Company.CompanyName} job posting via CareerCircle" : "Applicant Alert"),
+                                        RecruiterGuid = jobPosting.Recruiter.RecruiterGuid,
+                                        JobApplicationGuid = jobApplication.JobApplicationGuid
                                     },
-                                    new Attachment
+                                    Constants.SendGridAccount.Transactional,
+                                    null,
+                                    new List<Attachment>
                                     {
-                                        Content = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(applicationDto.CoverLetter)),
-                                        Filename = "CoverLetter.txt"
-                                    }
-                                },
-                                null,
-                                null
-                            ));
+                                        new Attachment
+                                        {
+                                            Content = resumeEncoded,
+                                            Filename = Path.GetFileName(subscriber.SubscriberFile.FirstOrDefault().BlobName),
+                                            Type=subscriber.SubscriberFile.FirstOrDefault().MimeType
+                                        },
+                                        new Attachment
+                                        {
+                                            Content = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(applicationDto.CoverLetter)),
+                                            Filename = "CoverLetter.txt"
+                                        }
+                                    },
+                                    null,
+                                    null
+                                ));
+                            }
+                            catch(Exception indemailsendex)
+                            {
+                                _syslog.Log(LogLevel.Information, "JOB APP INNER ABEND: "+indemailsendex.ToString());
+                            }
                         }
+                        catch(Exception hfex)
+                        {
+                            _syslog.Log(LogLevel.Information, "JOB APP OUTER ABEND: "+hfex.ToString());
+                        }
+                    }
                 }
                 catch(Exception ex)
                 {
