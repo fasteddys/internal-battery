f02a11e653f91a3be5b1eb5a1c227fbd4f573279
Brazil, Jim <jibrazil@populusgroup.com>
2020-03-30 10:40:59 -0400

    ready for dev pull

--- Product/UpDiddyLib/Domain/Models/G2InfoDTO.cs
+++ Product/UpDiddyLib/Domain/Models/G2InfoDTO.cs
@@ -22,45 +22,13 @@ namespace UpDiddyLib.Domain.Models
 
         public string PhoneNumber { get; set; }
 
-        public string ContactType { get; set; }
-
-        public string StreetAddress { get; set; }
-
         public string City { get; set; }
 
         public string State { get; set; }
 
-        public string Postal { get; set; }
+        public Guid SubscriberGuid { get; set; }
 
-        public string ExperienceLevel { get; set; }
-
-        public string EmploymentType { get; set; }
-
-        public string Title { get; set; }
-
-        public bool IsWillingToTravel { get; set; }
-
-        public bool IsActiveJobSeeker { get; set; }
-
-        public bool IsCurrentlyEmployed { get; set; }
-
-        public bool IsWillingToWorkProBono { get; set; }
-
-        public float CurrentRate { get; set; }
-
-        public float DesiredRate { get; set; }
-
-        public string Tags { get; set; }
-
-        public string Skills { get; set; }
-        public string SearchLocations { get; set; }
-
- 
-        public int CompanyId { get; set; }
-
-        public DateTime ModifyDate { get; set; }
-
-
+        public string AvatarUrl { get; set; }
 
     }
 }
