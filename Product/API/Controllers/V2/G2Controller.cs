b6fc55c18c486aff143baaa2b6cdf2a5e65dc961
Brazil, Jim <jibrazil@populusgroup.com>
2020-04-09 13:39:52 -0400

    code complete

--- Product/API/Controllers/V2/G2Controller.cs
+++ Product/API/Controllers/V2/G2Controller.cs
@@ -301,7 +301,7 @@ namespace UpDiddyApi.Controllers.V2
         [HttpGet]
         [Authorize(Policy = "IsRecruiterPolicy")]
         [Route("profiles/query")]
-        public async Task<IActionResult> SearchG2(Guid cityGuid, int limit = 10, int offset = 0, string sort = "ModifyDate", string order = "descending", string keyword = "*", Guid? sourcePartnerGuid = null, int radius = 0)
+        public async Task<IActionResult> SearchG2(Guid cityGuid, int limit = 10, int offset = 0, string sort = "ModifyDate", string order = "descending", string keyword = "*", Guid? sourcePartnerGuid = null, int radius = 0, bool? x = null)
         {
             var rVal = await _g2Service.G2SearchAsync(GetSubscriberGuid(), cityGuid, limit, offset, sort, order, keyword, sourcePartnerGuid, radius);
             return Ok(rVal);
@@ -310,43 +310,6 @@ namespace UpDiddyApi.Controllers.V2
         #endregion
 
         #region G2 Indexing Operations
-
-
-        // items requiring indexing 
-
-        // todo jab index        Mobile Phone 
-        // todo jab index Placement Type(Contract, Hourly, Full Time)
-        // todo jab index Willing to Relocate(Yes, No)
-
-
-
-
-
-
-
-        // todo jab index Course enrollment status
-        // todo jab index Course completion status
-
-        // todo jab put in migration for view changes 
-        // todo jab update azure index documentation 
-        // todo jab update DEV Gateway index paramenters 
-
-
-
-        // items requiring filters 
-
-        // todo jab filter on  Pro-bono assignments – searchable
-        // todo jab filter on  Willing to Travel
-        // todo jab filter on  IsActive Job Seeker 
-        // todo jab filter on IsCurrentlyEmployed 
-
-
-        // items requring searchable 
-
-        // todo jab index Certifications – searchable
-
-
-
         /// <summary>
         /// Re-index subsriber.  This operation will update as well as create documents in the 
         /// azure g2 index 
