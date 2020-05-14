e16c7e7d75cfd7d6cd84e6699fd3bbdcc7a0eba8
Brazil, Jim <jibrazil@populusgroup.com>
2020-05-14 07:03:18 -0400

    code complete

---
+++ Product/API/Migrations/20200514101725_sproc fix for System_Create_SubscriberG2Profiles.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UpDiddyApi.Migrations
+{
+    public partial class sprocfixforSystem_Create_SubscriberG2Profiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
