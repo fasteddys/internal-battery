67cb8117be76460ac405ea7ede1b75d2cc851d5a
Brazil, Jim <jibrazil@populusgroup.com>
2020-05-27 10:35:38 -0400

    Fixed endppoint to return correct data

--- Product/UpDiddyLib/Dto/EducationDto.cs
+++ Product/UpDiddyLib/Dto/EducationDto.cs
@@ -4,14 +4,16 @@ using System.Text;
 
 namespace UpDiddyLib.Dto
 {
+   
     public class EducationDto
 	{
-        public Guid? EducationHistoryGuid { get; set; }
-		public Guid? EducationalInstitutionGuid { get; set; }
+        public Guid? EducationHistoryGuid { get; set; }		
 		public DateTime? StartDate { get; set; }
 		public DateTime? EndDate { get; set; }
 		public DateTime? DegreeDate { get; set; }
-		public Guid? EducationalDegreeTypeGuid { get; set; }
-		public Guid? EducationalDegreeGuid { get; set; }
+	    public string Institution { get; set; }
+        public string EducationalDegree { get; set; }
     }
+
+
 }
