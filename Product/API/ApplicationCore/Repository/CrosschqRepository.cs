fced334a05a882f5b7c816de217095c44a3add3c
Vivek Dutta <vdutta@allegisgrroup.com>
2020-06-03 14:52:56 -0400

    Adding Crosschq repository objects

---
+++ Product/API/ApplicationCore/Repository/CrosschqRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using UpDiddyApi.ApplicationCore.Interfaces.Repository;
+using UpDiddyApi.Models;
+using UpDiddyApi.Models.G2;
+using UpDiddyLib.Dto;
+
+
+namespace UpDiddyApi.ApplicationCore.Repository
+{
+    public class CrosschqRepository : UpDiddyRepositoryBase<ReferenceCheck>, ICrosschqRepository
+    {
+        private readonly UpDiddyDbContext _dbContext;
+
+        public CrosschqRepository(UpDiddyDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
