07c8a0fbdeac86ff0994d78d56219852fc2e6aeb
Vivek Dutta <vdutta@allegisgrroup.com>
2020-06-12 17:28:12 -0400

    Candidate employment preference api endpoints and Dto.

--- Product/API/Controllers/V2/CandidatesController.cs
+++ Product/API/Controllers/V2/CandidatesController.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.Extensions.Logging;
 using UpDiddyApi.ApplicationCore.Interfaces.Business;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using UpDiddyApi.ApplicationCore.Interfaces.Business;
+using UpDiddyLib.Domain.Models;
 
 namespace UpDiddyApi.Controllers.V2
 {
+    [Route("v2/[controller]/")]
     public class CandidatesController : BaseApiController
     {
         private readonly ICandidatesService _candidatesService;
@@ -27,6 +34,32 @@ namespace UpDiddyApi.Controllers.V2
         #endregion Personal Info
 
         #region Employment Preferences
+
+        [HttpPost]
+        [Route("employment-preference/{subscriberGuid}")]
+        public async Task<IActionResult> AddCandidateEmploymentPreference(Guid subscriberGuid, [FromBody] CandidateEmploymentPreferenceDto request)
+        {
+            //await _candidatesService.AddCandidateEmploymentPreference(request);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("employment-preference/{subscriberGuid}")]
+        public async Task<IActionResult> GetCandidateEmploymentPreference(Guid subscriberGuid)
+        {
+            //await _candidatesService.GetCandidateEmploymentPreference(request);
+            return Ok(new CandidateEmploymentPreferenceDto());
+        }
+
+        [HttpPut]
+        [Route("employment-preference/{subscriberGuid}")]
+        public async Task<IActionResult> UpdateCandidateEmploymentPreference(Guid subscriberGuid, [FromBody] CandidateEmploymentPreferenceDto request)
+        {
+            //await _candidatesService.UpdateCandidateEmploymentPreference(request);
+            return Ok();
+        }
+
+
 
         #endregion Employment Preferences
 
---
+++ Product/UpDiddyLib/Domain/Models/CandidateEmploymentPreferenceDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UpDiddyLib.Domain.Models
+{
+    public class CandidateEmploymentPreferenceDto
+    {
+        /// <summary>
+        /// Subscriber guid of the candidate.
+        /// Used when the Dto returns a hydrated object.
+        /// </summary>
+        public Guid? CandidatesSubscriberGuid { get; set; }
+
+        public bool? IsWillingToTravel { get; set; }
+
+        public bool? IsFlexibleWorkScheduleRequired { get; set; }
+
+        /// <summary>
+        /// Guid that corresponds to CommuteDistance lookup.
+        /// </summary>
+        public Guid? CommuteDistanceGuid { get; set; }
+
+        /// <summary>
+        /// Guid that corresponds to a value in EmploymentType lookup.
+        /// </summary>
+        public List<Guid> PreferredPlacementTypes { get; set; }
+    }
+}
