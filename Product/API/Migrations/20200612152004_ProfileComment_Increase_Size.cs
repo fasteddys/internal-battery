37df0e56b2dfb4fa8f6719292adcecd0c5fdb43b
Joey Herrington <joharrington@allegisgroup.com>
2020-06-12 11:52:00 -0400

    #2515 Upped the Comments.Value max length from 500 to 2500

---
+++ Product/API/Migrations/20200612152004_ProfileComment_Increase_Size.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UpDiddyApi.Migrations
+{
+    public partial class ProfileComment_Increase_Size : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                schema: "G2",
+                table: "ProfileComments",
+                maxLength: 2500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                schema: "G2",
+                table: "ProfileComments",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 2500);
+        }
+    }
+}
