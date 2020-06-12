97e26c39a9ac4b8de92d7ffab607d93f25e9a65a
Joey Herrington <joharrington@allegisgroup.com>
2020-06-12 14:12:59 -0400

    #2480 #2481 #2482  Stubbed out controller and service classes since they are used in three stories, and will need to be edited simultaneously

---
+++ Product/API/ApplicationCore/Services/Candidate/CandidatesService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using UpDiddyApi.ApplicationCore.Interfaces.Business;
+
+namespace UpDiddyApi.ApplicationCore.Services.Candidate
+{
+    public class CandidatesService : ICandidatesService
+    {
+        private readonly ILogger _logger;
+
+        public CandidatesService(
+            ILogger<CandidatesService> logger
+            )
+        {
+            _logger = logger;
+        }
+
+        // This empty service class will be used for stories:
+        //   #2480 - Candidate 360: Personal Info
+        //   #2481 - Candidate 360: Employment Preferences
+        //   #2482 - Candidate 360: Role Preferences
+
+        #region Personal Info
+
+        #endregion Personal Info
+
+        #region Employment Preferences
+
+        #endregion Employment Preferences
+
+        #region Role Preferences
+
+        #endregion Role Preferences
+    }
+}
