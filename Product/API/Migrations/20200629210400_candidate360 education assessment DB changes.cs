3d1d502423828d19de0f068e60d5f36837a86c25
Vivek Dutta <vdutta@allegisgrroup.com>
2020-06-30 14:36:04 -0400

    C360 education Assessment lookup data upload scripts.

--- Product/API/Migrations/20200629210400_candidate360 education assessment DB changes.cs
+++ Product/API/Migrations/20200629210400_candidate360 education assessment DB changes.cs
@@ -152,6 +152,60 @@ namespace UpDiddyApi.Migrations
                 principalTable: "EducationalDegreeTypeCategory",
                 principalColumn: "EducationalDegreeTypeCategoryId",
                 onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.Sql(@"INSERT INTO [dbo].[EducationalDegreeTypeCategory] 
+                ([IsDeleted],[CreateDate],[ModifyDate],[CreateGuid],[ModifyGuid],[EducationalDegreeTypeCategoryGuid],[Name],[Sequence])
+                VALUES(0,GetUtcDate(),null,'00000000-0000-0000-0000-000000000000',null,'AB88F587-A02D-44DC-9668-FAC204CD3C8F','Secondary Education',1),
+                    (0,GetUtcDate(),null,'00000000-0000-0000-0000-000000000000',null,'BA1E68C0-8606-4C76-8E6E-07FBACE89EA9','Higher Education',2), 
+                    (0,GetUtcDate(),null,'00000000-0000-0000-0000-000000000000',null,'1BB6E6B4-C784-49B5-8365-7E22811FB4DB','Graduate',3)");
+
+            migrationBuilder.Sql(@"INSERT INTO [dbo].[TrainingType]
+                ([IsDeleted],[CreateDate],[ModifyDate],[CreateGuid],[ModifyGuid],[TrainingTypeGuid],[Name],[Sequence]) 
+                VALUES(0, GetUtcDate(), null, '00000000-0000-0000-0000-000000000000', null, 'E2CFECE9-BE36-4859-BB9A-93C972B6E18F', 'Technical Training', 1),
+	                  (0, GetUtcDate(), null, '00000000-0000-0000-0000-000000000000', null, '61708900-7419-42E0-92DA-A39668F4CF26', 'Professional Training', 2)");
+
+            migrationBuilder.Sql(@"Declare @EducationalDegreeTypeCategoryId int 
+                    SELECT @EducationalDegreeTypeCategoryId = [EducationalDegreeTypeCategoryId]
+                    FROM[dbo].[EducationalDegreeTypeCategory] 
+                    WHERE[Name] = 'Secondary Education' 
+                    INSERT INTO[dbo].[EducationalDegreeType] 
+                    ([IsDeleted],[CreateDate],[CreateGuid],[EducationalDegreeTypeGuid],[DegreeType],[EducationalDegreeTypeCategoryId],[IsVerified],[Sequence]) 
+                    VALUES 
+                      (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', 'F47FA511-D40E-4334-8D36-41022513AE5C', 'Currently Enrolled in High School', @EducationalDegreeTypeCategoryId, 1, 1),
+                      (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', '01FB224E-B0FD-4447-B66E-4F6692BC994C', 'High School Diploma', @EducationalDegreeTypeCategoryId, 1, 2),
+                      (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', 'F5FB7179-405F-4DDB-B2E2-4B8E09288769', 'GED', @EducationalDegreeTypeCategoryId, 1, 3),
+                      (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', '296DCBF9-7306-486B-90F5-FC6EBF620F47', 'Other', @EducationalDegreeTypeCategoryId, 1, 4)"
+                    );
+
+            migrationBuilder.Sql(@"Declare @EducationalDegreeTypeCategoryId int
+
+                SELECT @EducationalDegreeTypeCategoryId = [EducationalDegreeTypeCategoryId]
+                FROM [dbo].[EducationalDegreeTypeCategory]
+                WHERE [Name] = 'Higher Education'
+
+                INSERT INTO [dbo].[EducationalDegreeType]
+                ([IsDeleted],[CreateDate],[CreateGuid],[EducationalDegreeTypeGuid],[DegreeType],[EducationalDegreeTypeCategoryId],[IsVerified],[Sequence])
+                VALUES  
+                  (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', 'BF09D18C-7685-4AAD-B248-3E5BFF340A2C', 'Currently Enrolled in College',@EducationalDegreeTypeCategoryId, 1, 5),
+                  (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', '992AC43A-E5A9-4E99-89B3-BCECEF30C1C1', 'College Graduate',@EducationalDegreeTypeCategoryId, 1, 6),
+                  (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', '594677B4-06F7-4DE2-81F0-B94D54DA821F', 'Associates Degree',@EducationalDegreeTypeCategoryId, 1, 7),
+                  (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', 'DE11FA1E-53E5-4775-BA66-AD962E6B35B9', 'Completed Partial Coursework',@EducationalDegreeTypeCategoryId, 1, 8)
+                ");
+
+            migrationBuilder.Sql(@"Declare @EducationalDegreeTypeCategoryId int
+
+                SELECT @EducationalDegreeTypeCategoryId = [EducationalDegreeTypeCategoryId]
+                FROM [dbo].[EducationalDegreeTypeCategory]
+                WHERE [Name] = 'Graduate'
+
+                INSERT INTO [dbo].[EducationalDegreeType]
+                ([IsDeleted],[CreateDate],[CreateGuid],[EducationalDegreeTypeGuid],[DegreeType],[EducationalDegreeTypeCategoryId],[IsVerified],[Sequence])
+                VALUES  
+                  (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', '7F36713A-1EB7-41D0-AFCE-28FEAA2AD196', 'Currently Enrolled in Graduate Program',@EducationalDegreeTypeCategoryId, 1, 9),
+                  (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', '2DDAC994-BFA5-4213-A322-8E382CC8BABF', 'Master''s Degree Graduation',@EducationalDegreeTypeCategoryId, 1, 10),
+                  (0, GetUtcDate(), '00000000-0000-0000-0000-000000000000', 'AA4B5B9D-2F1D-4FD5-BEDB-8297CC2127EA', 'Completed Partial Coursework',@EducationalDegreeTypeCategoryId, 1, 11)
+                ");
+
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
