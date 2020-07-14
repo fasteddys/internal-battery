7961ff2d0eea3a5e7124ed0422e6e941b7aff34b
Vivek Dutta <vdutta@allegisgrroup.com>
2020-07-14 14:56:07 -0400

    updated code

--- Product/API/ApplicationCore/Services/Admin/AccountManagementService.cs
+++ Product/API/ApplicationCore/Services/Admin/AccountManagementService.cs
@@ -6,6 +6,7 @@ using UpDiddyApi.ApplicationCore.Exceptions;
 using UpDiddyApi.ApplicationCore.Interfaces;
 using UpDiddyApi.ApplicationCore.Interfaces.Business;
 using UpDiddyApi.ApplicationCore.Interfaces.Repository;
+using UpDiddyApi.ApplicationCore.Services.Identity.Interfaces;
 using UpDiddyLib.Dto.User;
 using Auth0.ManagementApi.Models;
 
@@ -17,16 +18,19 @@ namespace UpDiddyApi.ApplicationCore.Services.Admin
         private ILogger _logger { get; set; }
         private IRepositoryWrapper _repository { get; set; }
         private readonly IMapper _mapper;
+        private IUserService _userService { get;set; }
 
         public AccountManagementService(
             ILogger<AccountManagementService> logger,
             IRepositoryWrapper repository,
-            IMapper mapper
+            IMapper mapper,
+            IUserService userService
             )
         {
             _logger = logger;
             _repository = repository;
             _mapper = mapper;
+            _userService = userService;
         }
 
         public async Task<UserStatsDto> GetUserStatsByEmail(string email)
@@ -57,7 +61,6 @@ namespace UpDiddyApi.ApplicationCore.Services.Admin
             }
 
             _logger.LogInformation($"AccountManagementService:GetUserStatsByEmail end.");
-            throw new NotImplementedException();
         }
 
         public async Task<bool> GetAuth0VerificationStatus(Guid subscriberGuid)
@@ -70,8 +73,9 @@ namespace UpDiddyApi.ApplicationCore.Services.Admin
                 var subscriber = await _repository.SubscriberRepository.GetSubscriberAccountDetailsByGuidAsync(subscriberGuid);
                 if (subscriber == null)
                     throw new NotFoundException($"SubscriberGuid {subscriberGuid} does not exist exist");
+                var user = await _userService.GetUserByEmailAsync(subscriber.Email);
 
-                return subscriber.EmailVerification != null;
+                return user.User?.EmailVerified ?? false;
             }
             catch (Exception ex)
             {
@@ -80,7 +84,6 @@ namespace UpDiddyApi.ApplicationCore.Services.Admin
             }
 
             _logger.LogInformation($"AccountManagementService:GetAuth0VerificationStatus end.");
-            throw new NotImplementedException();
         }
 
         public async Task ForceVerification(Guid subscriberGuid)
