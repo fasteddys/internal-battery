81b451fab0882bbcaa661774f11ad6e655ba52ce
Vivek Dutta <vdutta@allegisgrroup.com>
2020-08-04 08:51:23 -0400

    Bug fixes and code cleanup.

---
+++ Product/API/Migrations/20200804013329_Track DB model fixes.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UpDiddyApi.Migrations
+{
+    public partial class TrackDBmodelfixes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "UIX_Tracking_SourceSlug",
+                table: "SubscriberEmploymentTypes",
+                newName: "UIX_SubscriberEmploymentTypes_Subscriber_EmploymentType");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SourceSlug",
+                table: "Tracking",
+                type: "VARCHAR(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(50)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UIX_Tracking_SourceSlug",
+                table: "Tracking",
+                column: "SourceSlug",
+                unique: true,
+                filter: "[SourceSlug] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "UIX_Tracking_SourceSlug",
+                table: "Tracking");
+
+            migrationBuilder.RenameIndex(
+                name: "UIX_SubscriberEmploymentTypes_Subscriber_EmploymentType",
+                table: "SubscriberEmploymentTypes",
+                newName: "UIX_Tracking_SourceSlug");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SourceSlug",
+                table: "Tracking",
+                type: "VARCHAR(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(150)",
+                oldNullable: true);
+        }
+    }
+}
